f471b7b1bfada9dda9e5f25b9b962d5287e4abf4
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2018-07-29 20:19:43 +0300

    make pep561 compliant

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from distutils.core import setup
+
+
+def find_stubs(package):
+    stubs = []
+    for root, dirs, files in os.walk(package):
+        for file in files:
+            path = os.path.join(root, file).replace(package + os.sep, '', 1)
+            stubs.append(path)
+    return {package: stubs}
+
+
+setup(
+    name="django-stubs",
+    url="https://github.com/mkurnikov/django-stubs.git",
+    author="Maksim Kurnikov",
+    author_email="maxim.kurnikov@gmail.com",
+    version="0.1.0",
+    license='BSD',
+    install_requires='Django==2.1rc1',
+    packages=['numpy-stubs'],
+    package_data=find_stubs('django-stubs')
+)
