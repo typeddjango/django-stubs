c7066fe289d3679bc9e57dbbb7da5670f5afad7a
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2018-11-13 19:05:31 +0300

    add stubs for models.Model method

--- django-stubs/db/models/base.pyi
+++ django-stubs/db/models/base.pyi
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List, Optional, Set
 
 
 class ModelBase(type):
@@ -9,8 +9,32 @@ class Model(metaclass=ModelBase):
     class DoesNotExist(Exception):
         pass
 
+    pk: Any = ...
+
     def __init__(self, **kwargs) -> None: ...
 
     def delete(self,
                using: Any = ...,
                keep_parents: bool = ...) -> None: ...
+
+    def full_clean(
+        self, exclude: Optional[List[str]] = ..., validate_unique: bool = ...
+    ) -> None: ...
+
+    def clean_fields(self, exclude: List[str] = ...) -> None: ...
+
+    def validate_unique(self, exclude: List[str] = ...) -> None: ...
+
+    def save(
+        self,
+        force_insert: bool = ...,
+        force_update: bool = ...,
+        using: Optional[str] = ...,
+        update_fields: Optional[List[str]] = ...,
+    ) -> None: ...
+
+    def refresh_from_db(
+        self, using: None = ..., fields: Optional[List[str]] = ...
+    ) -> None: ...
+
+    def get_deferred_fields(self) -> Set[str]: ...
