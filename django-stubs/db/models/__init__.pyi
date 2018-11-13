4f828f76443c4c3820b6441c4321fee2d3440b83
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2018-11-13 18:39:39 +0300

    add Q object

--- django-stubs/db/models/__init__.pyi
+++ django-stubs/db/models/__init__.pyi
@@ -14,3 +14,4 @@ from .deletion import (CASCADE as CASCADE,
                        SET_NULL as SET_NULL,
                        DO_NOTHING as DO_NOTHING)
 from .query import QuerySet as QuerySet
+from .query_utils import Q as Q
