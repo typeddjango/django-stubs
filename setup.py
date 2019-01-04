a3206e8884f60d03cf9ea4b2f64c86873cd266f9
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-01-05 02:04:40 +0300

    fix non-editable installation

--- setup.py
+++ setup.py
@@ -1,15 +1,39 @@
+import os
 from distutils.core import setup
+
+from setuptools import find_packages
+
+
+def find_stub_files(name):
+    result = []
+    for root, dirs, files in os.walk(name):
+        for file in files:
+            if file.endswith('.pyi'):
+                if os.path.sep in root:
+                    sub_root = root.split(os.path.sep, 1)[-1]
+                    file = os.path.join(sub_root, file)
+                result.append(file)
+    return result
+
 
 setup(
     name="django-stubs",
+    description='Django mypy stubs',
+    version="0.1.0",
+    license='BSD',
     url="https://github.com/mkurnikov/django-stubs.git",
     author="Maksim Kurnikov",
     author_email="maxim.kurnikov@gmail.com",
-    version="0.1.0",
-    license='BSD',
+    py_modules=[],
+    python_requires='>=3',
     install_requires=[
         'Django',
         'mypy @ git+https://github.com/python/mypy.git#egg=mypy-0.660+dev.a7296c4595350768ec8ecf145c23a76b6c98e8e6'
     ],
-    packages=['mypy_django_plugin']
+    packages=['django-stubs', *find_packages()],
+    package_data={'django-stubs': find_stub_files('django-stubs')},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3'
+    ]
 )
