cff5ab463c911283a9c43a26a38cb7bd4deebbd5
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-01-22 20:13:31 +0300

    fix tests

--- mypy_django_plugin/monkeypatch/__init__.py
+++ mypy_django_plugin/monkeypatch/__init__.py
@@ -3,4 +3,3 @@ from .dependencies import (load_graph_to_add_settings_file_as_a_source_seed,
                            restore_original_load_graph,
                            restore_original_dependencies_handling,
                            process_settings_before_dependants)
-from .multiple_inheritance import make_inner_classes_with_inherit_from_any_compatible_with_each_other
