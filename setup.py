194489ee8d525f57e83ff99d6b7ca9f937aa1e0c
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-03-05 20:21:27 +0300

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if sys.version_info[:2] < (3, 7):
 
 setup(
     name="django-stubs",
-    version="0.8.0",
+    version="0.8.1",
     description='Django mypy stubs',
     long_description=readme,
     long_description_content_type='text/markdown',
