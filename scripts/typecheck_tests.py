3495cef5848da1a17a04368389575a4a25968bda
Seth Yastrov <syastrov@gmail.com>
2019-03-08 08:19:44 +0100

    Updated ignored errors in Django test for get_objects_or_404.
    - For some reason, `Manager[nothing]` is now removed from expected types.
      However, I think this makes sense anyway, as Manager is a subclass of QuerySet.

--- scripts/typecheck_tests.py
+++ scripts/typecheck_tests.py
@@ -199,9 +199,9 @@ IGNORED_ERRORS = {
     ],
     'get_object_or_404': [
         'Argument 1 to "get_object_or_404" has incompatible type "str"; '
-        + 'expected "Union[Type[<nothing>], Manager[<nothing>], QuerySet[<nothing>]]"',
+        + 'expected "Union[Type[<nothing>], QuerySet[<nothing>]]"',
         'Argument 1 to "get_list_or_404" has incompatible type "List[Type[Article]]"; '
-        + 'expected "Union[Type[<nothing>], Manager[<nothing>], QuerySet[<nothing>]]"',
+        + 'expected "Union[Type[<nothing>], QuerySet[<nothing>]]"',
         'CustomClass'
     ],
     'get_or_create': [
