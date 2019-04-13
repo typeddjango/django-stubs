d4cbaa3d245a12a829a79ebc8a6ba443667f7ddc
maciej-gol <1kroolik1@gmail.com>
2019-04-13 16:14:23 +0200

    Preserve callable type in view decorators

--- django-stubs/views/decorators/http.pyi
+++ django-stubs/views/decorators/http.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, List, Optional, TypeVar
 
 conditional_page: Any
 
@@ -8,6 +8,8 @@ require_GET: Any
 require_POST: Any
 require_safe: Any
 
+_F = TypeVar("_F", bound=Callable[..., Any])
+
 def condition(etag_func: Optional[Callable] = ..., last_modified_func: Optional[Callable] = ...) -> Callable: ...
-def etag(etag_func: Callable) -> Callable: ...
-def last_modified(last_modified_func: Callable) -> Callable: ...
+def etag(etag_func: Callable[..., Any]) -> Callable[[_F], _F]: ...
+def last_modified(last_modified_func: Callable[..., Any]) -> Callable[[_F], _F]: ...
