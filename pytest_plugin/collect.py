bba6f769b580875dbdd1b069371c4a9d74396830
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-07-18 19:36:56 +0300

    add django.contrib.contenttypes to all tests by default

--- pytest_plugin/collect.py
+++ pytest_plugin/collect.py
@@ -26,10 +26,8 @@ class NewSemanalDjangoTestItem(YamlTestItem):
 
         installed_apps = self.parsed_test_data.get('installed_apps', None)
         if installed_apps is not None:
-            if not installed_apps:
-                installed_apps_as_str = '()'
-            else:
-                installed_apps_as_str = '(' + ','.join([repr(app) for app in installed_apps]) + ',)'
+            installed_apps += ['django.contrib.contenttypes']
+            installed_apps_as_str = '(' + ','.join([repr(app) for app in installed_apps]) + ',)'
 
             pyproject_toml_file = File(path='pyproject.toml',
                                        content='[tool.django-stubs]\ndjango_settings_module=\'mysettings\'')
