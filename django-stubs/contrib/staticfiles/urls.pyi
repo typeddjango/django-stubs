ff7bf33e9c15c1cc30666320cbb4e9485b707cd5
Nikita Sobolev <mail@sobolevn.me>
2019-08-16 15:45:17 +0300

    Fixes #127, adds urls.pyi for several contrib apps (#129)

--- django-stubs/contrib/staticfiles/urls.pyi
+++ django-stubs/contrib/staticfiles/urls.pyi
@@ -2,6 +2,6 @@ from typing import Any, List, Optional
 
 from django.urls.resolvers import URLPattern
 
-urlpatterns: Any
+urlpatterns: List[Any] = ...
 
 def staticfiles_urlpatterns(prefix: Optional[str] = ...) -> List[URLPattern]: ...
