e95b40ef524f5a11bae0050d807bda05c10e55cd
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-08-24 17:04:50 +0300

    stability fixes

--- mypy_django_plugin/transformers/models.py
+++ mypy_django_plugin/transformers/models.py
@@ -100,7 +100,8 @@ class AddRelatedModelsId(ModelClassInitializer):
     def run_with_model_cls(self, model_cls: Type[Model]) -> None:
         for field in model_cls._meta.get_fields():
             if isinstance(field, ForeignKey):
-                rel_primary_key_field = self.django_context.get_primary_key_field(field.related_model)
+                related_model_cls = self.django_context.fields_context.get_related_model_cls(field)
+                rel_primary_key_field = self.django_context.get_primary_key_field(related_model_cls)
                 field_info = self.lookup_class_typeinfo_or_incomplete_defn_error(rel_primary_key_field.__class__)
                 is_nullable = self.django_context.fields_context.get_field_nullability(field, None)
                 set_type, get_type = get_field_descriptor_types(field_info, is_nullable)
@@ -156,7 +157,8 @@ class AddManagers(ModelClassInitializer):
                 # no reverse accessor
                 continue
 
-            related_model_info = self.lookup_class_typeinfo_or_incomplete_defn_error(relation.related_model)
+            related_model_cls = self.django_context.fields_context.get_related_model_cls(relation)
+            related_model_info = self.lookup_class_typeinfo_or_incomplete_defn_error(related_model_cls)
 
             if isinstance(relation, OneToOneRel):
                 self.add_new_node_to_model_class(attname, Instance(related_model_info, []))
