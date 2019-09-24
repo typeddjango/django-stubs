5cf12e08c211ee3f4a74647aa39d885a435c4aec
Nick Sweeting <git@sweeting.me>
2019-09-24 15:59:20 -0400

    fix black error forreals this time

--- django-stubs/core/paginator.pyi
+++ django-stubs/core/paginator.pyi
@@ -1,14 +1,4 @@
-from typing import (
-    Any,
-    Dict,
-    List,
-    Optional,
-    Union,
-    Iterable,
-    Sequence,
-    Protocol,
-    Callable,
-)
+from typing import Any, Dict, List, Optional, Union, Iterable, Sequence, Protocol, Callable
 
 from django.db.models.base import Model
 from django.db.models.query import QuerySet
