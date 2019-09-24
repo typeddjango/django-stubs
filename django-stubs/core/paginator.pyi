7fc62ee65234d53820b31b1ab6bf37d5588f0f21
Nick Sweeting <git@sweeting.me>
2019-09-24 18:14:46 -0400

    Remove unused import

--- django-stubs/core/paginator.pyi
+++ django-stubs/core/paginator.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Union, Iterable, Sequence, Protocol, Callable
+from typing import Dict, List, Optional, Union, Iterable, Sequence, Protocol, Any
 
 from django.db.models.base import Model
 from django.db.models.query import QuerySet
