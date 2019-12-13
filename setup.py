f7e1cfc6c7016eff604d0eacf6bd6fa9a75eccc3
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-12-13 23:55:08 +0300

    bump to 1.3.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ dependencies = [
 
 setup(
     name="django-stubs",
-    version="1.3.1",
+    version="1.3.2",
     description='Mypy stubs for Django',
     long_description=readme,
     long_description_content_type='text/markdown',
