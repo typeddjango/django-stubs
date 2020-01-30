213991a20f62a1f17d406be9e8113ad7a98447d4
Konstantin Alekseev <mail@kalekseev.com>
2020-01-30 12:49:07 +0300

    Cleanup checks framework

--- django-stubs/core/checks/__init__.pyi
+++ django-stubs/core/checks/__init__.pyi
@@ -1,5 +1,17 @@
-from .messages import Warning as Warning, Info as Info, Debug as Debug, Error as Error, Critical as Critical
+from .messages import (
+    CheckMessage as CheckMessage,
+    Debug as Debug,
+    Info as Info,
+    Warning as Warning,
+    Error as Error,
+    Critical as Critical,
+    DEBUG as DEBUG,
+    INFO as INFO,
+    WARNING as WARNING,
+    ERROR as ERROR,
+    CRITICAL as CRITICAL,
+)
 
-from .registry import run_checks as run_checks, Tags as Tags, register as register
+from .registry import register as register, run_checks as run_checks, tag_exists as tag_exists, Tags as Tags
 
 from . import model_checks as model_checks
