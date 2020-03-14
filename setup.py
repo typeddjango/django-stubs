1af3a12f2c7df2513d5a80d28edc80c8376a1e93
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2020-03-15 00:59:58 +0300

    bump to 1.5.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ dependencies = [
 
 setup(
     name="django-stubs",
-    version="1.4.0",
+    version="1.5.0",
     description='Mypy stubs for Django',
     long_description=readme,
     long_description_content_type='text/markdown',
