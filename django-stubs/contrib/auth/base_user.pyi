acd2ea4cf67d7a2e85cd667e4436ebf0d95c75cd
Kacper Szmigiel <szmigielkacper@gmai.com>
2020-05-12 22:34:22 +0200

    checking python version fix for readibility

--- django-stubs/contrib/auth/base_user.pyi
+++ django-stubs/contrib/auth/base_user.pyi
@@ -5,7 +5,7 @@ from django.db.models.base import Model
 
 from django.db import models
 
-if sys.version_info[1] < 8:
+if sys.version_info < (3, 8):
     from typing_extensions import Literal
 else:
     from typing import Literal
