4b6daa67e3a2ff95ed9b80dd521693410a79d739
Kacper Szmigiel <szmigielkacper@gmai.com>
2020-06-01 18:26:26 +0200

    typo

--- django-stubs/shortcuts.pyi
+++ django-stubs/shortcuts.pyi
@@ -20,7 +20,7 @@ def render_to_response(
 def render(
     request: HttpRequest,
     template_name: Union[str, Sequence[str]],
-    context: Optional[ Mapping[str, Any]] = ...,
+    context: Optional[Mapping[str, Any]] = ...,
     content_type: Optional[str] = ...,
     status: Optional[int] = ...,
     using: Optional[str] = ...,
