8390de3f337b7486a8237c96c29e92698cb4b383
Kacper Szmigiel <szmigielkacper@gmai.com>
2020-08-03 17:30:12 +0200

    unused type ignore comment

--- mypy_django_plugin/lib/helpers.py
+++ mypy_django_plugin/lib/helpers.py
@@ -136,7 +136,7 @@ class SemanalPluginCallback(DjangoPluginCallback):
                 method_node.arguments[1:]):
             bound_arg_type = self.semanal_api.anal_type(arg_type, allow_placeholder=True)
             if bound_arg_type is None and not self.defer_till_next_iteration(reason='bound_arg_type is None'):
-                error_msg = 'of {} argument of {}'.format(arg_name, method_node.fullname)  # type: ignore
+                error_msg = 'of {} argument of {}'.format(arg_name, method_node.fullname)
                 raise new_helpers.TypeInfoNotFound(error_msg)
 
             assert bound_arg_type is not None
