cff34fa61e45ce78cb065fe60de9fc23a20d6100
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2020-10-13 12:42:00 +0200

    updated to use python 3.9

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Typing :: Typed",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
