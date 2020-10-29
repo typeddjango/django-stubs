d3d854dac83a2295662cb651c076f3d77116e07b
James Pulec <jpulec@gmail.com>
2020-10-28 20:27:46 -0700

    Add resolve_callables to db.models.utils

--- django-stubs/db/models/utils.pyi
+++ django-stubs/db/models/utils.pyi
@@ -1,5 +1,7 @@
-from typing import Tuple, Type, Union
+from typing import Any, Generator, MutableMapping, Tuple, Type, Union
 
 from django.db.models.base import Model
 
 def make_model_tuple(model: Union[Type[Model], str]) -> Tuple[str, str]: ...
+  
+def resolve_callables(mapping: MutableMapping[str, Any]) -> Generator[Tuple[str, Any]]: ...
