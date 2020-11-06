1c4a7d25c7d1ad521e03efa3e76fb26d4be2b5a6
Clarity <clarity@ridereportapp.com>
2020-11-06 14:34:47 -0800

    Add Point and Polygon to contrib.gis.geos (#524)

--- django-stubs/contrib/gis/geos/__init__.pyi
+++ django-stubs/contrib/gis/geos/__init__.pyi
@@ -8,3 +8,5 @@ from .factory import fromfile as fromfile, fromstr as fromstr
 from .geometry import GEOSGeometry as GEOSGeometry, hex_regex as hex_regex, wkt_regex as wkt_regex
 from .io import WKBReader as WKBReader, WKBWriter as WKBWriter, WKTReader as WKTReader, WKTWriter as WKTWriter
 from .linestring import LineString as LineString, LinearRing as LinearRing
+from .point import Point as Point
+from .polygon import Polygon as Polygon
