a4736ab0394fcca4d40a83720686b10b1d8ca1f1
proxi <51172302+3n-k1@users.noreply.github.com>
2020-11-06 18:11:26 -0500

    properly import test client

--- django-stubs/contrib/auth/__init__.pyi
+++ django-stubs/contrib/auth/__init__.pyi
@@ -3,12 +3,11 @@ from typing import Any, List, Optional, Type, Union
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import AnonymousUser
-from django.contrib.sessions.backends.base import SessionBase
 from django.core.handlers.wsgi import WSGIRequest
 from django.db.models.base import Model
 from django.db.models.options import Options
 from django.http.request import HttpRequest
-from django.test import Client
+from django.test.client import Client
 
 from .signals import user_logged_in as user_logged_in
 from .signals import user_logged_out as user_logged_out
