2c5b6d69b431f89b32b71932117d52b585e2b1c4
Tim Martin <tim@asymptotic.co.uk>
2020-11-08 16:00:42 +0000

    Tighten up stub definitions in django.core.management.commands

--- django-stubs/core/management/commands/check.pyi
+++ django-stubs/core/management/commands/check.pyi
@@ -2,7 +2,7 @@ from django.apps import apps as apps
 from django.core import checks as checks
 from django.core.checks.registry import registry as registry
 from django.core.management.base import BaseCommand as BaseCommand, CommandError as CommandError
-from typing import Any
+from typing import Any, List
 
 class Command(BaseCommand):
-    def handle(self, *app_labels: Any, **options: Any) -> None: ...
+    def handle(self, *app_labels: List[str], **options: Any) -> None: ...
