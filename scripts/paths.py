60b286cb203d61e909a4e4462be166dde83ec0bd
Steve Dignam <steve@dignam.xyz>
2020-11-25 20:53:10 -0500

    delete unused scripts, fix setup.py

--- scripts/paths.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-PROJECT_DIRECTORY = Path(__file__).parent.parent
-DJANGO_SOURCE_DIRECTORY = PROJECT_DIRECTORY / "django-source"  # type: Path
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages
+from setuptools import setup
 
 # This allows for the pip install -e . to work so that mypy can check the types
 # of the examples. Since the stubs aren't using valid python package names,
