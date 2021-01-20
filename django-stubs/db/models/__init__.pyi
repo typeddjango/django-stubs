a8d8561d0e6579466a1e0adeddb3476924d24455
Nicolas Delaby <nicolas.delaby@infarm.com>
2021-01-20 09:04:38 +0100

    Add deferrable argument to unique constraint (#472)

--- django-stubs/db/models/__init__.pyi
+++ django-stubs/db/models/__init__.pyi
@@ -132,6 +132,7 @@ from . import signals as signals
 from .constraints import (
     BaseConstraint as BaseConstraint,
     CheckConstraint as CheckConstraint,
+    Deferrable as Deferrable,
     UniqueConstraint as UniqueConstraint,
 )
 
