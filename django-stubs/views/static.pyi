993dd816a4ddf0048f69c9a114de6183d23ffd31
Thomas Krapp <t.krapp@googlemail.com>
2021-02-12 22:27:12 +0100

    Update static.pyi (#566)
    * Update static.pyi
    
    Parameters document_root and show_indexes should be marked as Optional since they have a default value.
    
    * Update static.pyi
    
    Correct signature of serve function.

--- django-stubs/views/static.pyi
+++ django-stubs/views/static.pyi
@@ -3,7 +3,7 @@ from typing import Any, Optional
 from django.http.request import HttpRequest
 from django.http.response import FileResponse
 
-def serve(request: HttpRequest, path: str, document_root: str = ..., show_indexes: bool = ...) -> FileResponse: ...
+def serve(request: HttpRequest, path: str, document_root: Optional[str] = ..., show_indexes: bool = ...) -> FileResponse: ...
 
 DEFAULT_DIRECTORY_INDEX_TEMPLATE: str
 template_translatable: Any
