d59e8905eb54bd7913db9e2ab2b8d41e61d74929
Steve Dignam <steve@dignam.xyz>
2021-02-26 13:57:13 -0500

    remove: python 3.7 requirement
    Poetry defaults to the system version, 3.7 in this case, which was preventing
    users of 3.6 from installing the package.

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-# This allows for the pip install -e . to work so that mypy can check the types
-# of the examples. Since the stubs aren't using valid python package names,
-# mypy can't check the types normally.
-
-setup(name="django-stubs")
