9287f2f38135f97e9ac9b9e2f37ce8fcdd2e7d20
Marti Raudsepp <marti@juffo.org>
2021-05-13 15:00:15 +0300

    Improve hints for database connections (DatabaseWrapper)
    * `django.db.{connection, connections, router}` are now hinted -- including `ConnectionHandler` and `ConnectionRouter` classes.
    * Several improvements to `BaseDatabaseWrapper` attribute hints.
    * In many places, database connections were hinted as `Any`, which I changed to `BaseDatabaseWrapper`.
    * In a few places I added additional `SQLCompiler` hints.
    * Minor tweaks to nearby code.

--- django-stubs/core/management/sql.pyi
+++ django-stubs/core/management/sql.pyi
@@ -1,9 +1,14 @@
 from typing import Any, List
 
 from django.core.management.color import Style
+from django.db.backends.base.base import BaseDatabaseWrapper
 
 def sql_flush(
-    style: Style, connection: Any, only_django: bool = ..., reset_sequences: bool = ..., allow_cascade: bool = ...
+    style: Style,
+    connection: BaseDatabaseWrapper,
+    only_django: bool = ...,
+    reset_sequences: bool = ...,
+    allow_cascade: bool = ...,
 ) -> List[str]: ...
 def emit_pre_migrate_signal(verbosity: int, interactive: bool, db: str, **kwargs: Any) -> None: ...
 def emit_post_migrate_signal(verbosity: int, interactive: bool, db: str, **kwargs: Any) -> None: ...
