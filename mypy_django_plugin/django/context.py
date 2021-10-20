306cbe6b69cab2ba64e6be5987fe217af1c9ab5a
Zach Waggoner <zach.waggoner1@gmail.com>
2021-10-20 13:01:10 -0500

    Don't treat CharFields with blank=True as nullable

--- mypy_django_plugin/django/context.py
+++ mypy_django_plugin/django/context.py
@@ -245,8 +245,6 @@ class DjangoContext:
 
     def get_field_nullability(self, field: Union[Field, ForeignObjectRel], method: Optional[str]) -> bool:
         nullable = field.null
-        if not nullable and isinstance(field, CharField) and field.blank:
-            return True
         if method == "__init__":
             if (isinstance(field, Field) and field.primary_key) or isinstance(field, ForeignKey):
                 return True
