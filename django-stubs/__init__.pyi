82641613958a17b94e93dab6fb7c0c56e1ec9584
Seth Yastrov <syastrov@gmail.com>
2021-11-18 15:35:12 +0100

    Attempt to fix 'INTERNAL ERROR using annotate'.
    KeyError: "Failed qualified lookup: '_AnyAttrAllowed' (fullname =
    'django._AnyAttrAllowed')."
    
    https://github.com/typeddjango/django-stubs/issues/745

--- django-stubs/__init__.pyi
+++ django-stubs/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Protocol
+from typing import Any
 
 from .utils.version import get_version as get_version
 
@@ -6,8 +6,3 @@ VERSION: Any
 __version__: str
 
 def setup(set_prefix: bool = ...) -> None: ...
-
-# Used internally by mypy_django_plugin.
-class _AnyAttrAllowed(Protocol):
-    def __getattr__(self, item: str) -> Any: ...
-    def __setattr__(self, item: str, value: Any) -> None: ...
--- django_stubs_ext/django_stubs_ext/__init__.py
+++ django_stubs_ext/django_stubs_ext/__init__.py
@@ -2,5 +2,6 @@ from .aliases import ValuesQuerySet as ValuesQuerySet
 from .annotations import Annotations as Annotations
 from .annotations import WithAnnotations as WithAnnotations
 from .patch import monkeypatch as monkeypatch
+from .types import AnyAttrAllowed as AnyAttrAllowed
 
-__all__ = ["monkeypatch", "ValuesQuerySet", "WithAnnotations", "Annotations"]
+__all__ = ["monkeypatch", "ValuesQuerySet", "WithAnnotations", "Annotations", "AnyAttrAllowed"]
