3ffdf9b06d06fdc7f5c0c4584cbdc14c7326a80b
Kevin Marsh <kevinmarsh3@gmail.com>
2022-02-09 15:15:42 -0800

    DatabaseClient: make `settings_dict` dict type better by specifying key/value types

--- django-stubs/db/backends/mysql/client.pyi
+++ django-stubs/db/backends/mysql/client.pyi
@@ -1,4 +1,4 @@
-from typing import Dict, Iterable, List, Optional, Tuple, Union
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
 
 from django.db.backends.base.client import BaseDatabaseClient
 
@@ -6,5 +6,5 @@ class DatabaseClient(BaseDatabaseClient):
     executable_name: str = ...
     @classmethod
     def settings_to_cmd_args_env(
-        self, settings_dict: dict, parameters: Iterable[str]
-    ) -> Tuple[List[str], Optional[dict]]: ...
+        self, settings_dict: Dict[str, Any], parameters: Iterable[str]
+    ) -> Tuple[List[str], Optional[Dict[str, str]]]: ...
--- django-stubs/db/backends/sqlite3/client.pyi
+++ django-stubs/db/backends/sqlite3/client.pyi
@@ -1,8 +1,10 @@
-from typing import Iterable, List, Tuple
+from typing import Any, Dict, Iterable, List, Tuple
 
 from django.db.backends.base.client import BaseDatabaseClient as BaseDatabaseClient
 
 class DatabaseClient(BaseDatabaseClient):
     executable_name: str = ...
     @classmethod
-    def settings_to_cmd_args_env(self, settings_dict: dict, parameters: Iterable[str]) -> Tuple[List[str], None]: ...
+    def settings_to_cmd_args_env(
+        self, settings_dict: Dict[str, Any], parameters: Iterable[str]
+    ) -> Tuple[List[str], None]: ...
