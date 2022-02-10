3eab36a5c4af0500e2d66e7e4e958112c904fb28
Kevin Marsh <kevinmarsh@users.noreply.github.com>
2022-02-09 22:29:44 -0800

    DatabaseClient: add missing class methods and reflect Django 3.2 refactor (#846)
    * DatabaseClient: add missing class method `settings_to_cmd_args_env` and add missing arg to `runshell`
    
    The `DatabaseClient.runshell()` was refactored in Django 3.2 to be more standardized
    across the different database backends.
    
    * DatabaseClient: make `settings_dict` dict type better by specifying key/value types
    
    * tests: add missing db backend client files to `import_all_modules`

--- django-stubs/db/backends/oracle/client.pyi
+++ django-stubs/db/backends/oracle/client.pyi
@@ -1,5 +1,13 @@
+from typing import Any, Dict, Iterable, List, Tuple
+
 from django.db.backends.base.client import BaseDatabaseClient as BaseDatabaseClient
 
 class DatabaseClient(BaseDatabaseClient):
     executable_name: str = ...
     wrapper_name: str = ...
+    @staticmethod
+    def connect_string(settings_dict: Dict[str, Any]) -> str: ...
+    @classmethod
+    def settings_to_cmd_args_env(
+        self, settings_dict: Dict[str, Any], parameters: Iterable[str]
+    ) -> Tuple[List[str], None]: ...
--- django-stubs/db/backends/postgresql/client.pyi
+++ django-stubs/db/backends/postgresql/client.pyi
@@ -1,9 +1,10 @@
-from typing import Dict
+from typing import Any, Dict, Iterable, List, Optional, Tuple
 
 from django.db.backends.base.client import BaseDatabaseClient
 
 class DatabaseClient(BaseDatabaseClient):
     executable_name: str = ...
     @classmethod
-    def runshell_db(cls, conn_params: Dict[str, str]) -> None: ...
-    def runshell(self) -> None: ...
+    def settings_to_cmd_args_env(
+        self, settings_dict: Dict[str, Any], parameters: Iterable[str]
+    ) -> Tuple[List[str], Optional[Dict[str, str]]]: ...
