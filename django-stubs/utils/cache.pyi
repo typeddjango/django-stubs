f4e32fdf6c87c63bae3511feb701fde64e78b6bd
Joon Hwan 김준환 <xncbf12@gmail.com>
2022-02-14 14:29:57 +0900

    Update cache.pyi

--- django-stubs/utils/cache.pyi
+++ django-stubs/utils/cache.pyi
@@ -1,7 +1,9 @@
-from typing import Any, Optional, Tuple
+from typing import Any, Optional, Tuple, Union
 
 from django.core.cache.backends.base import BaseCache
+from django.core.handlers.asgi import ASGIRequest
 from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpRequest
 from django.http.response import HttpResponse, HttpResponseBase
 
 cc_delim_re: Any
@@ -20,7 +22,10 @@ def add_never_cache_headers(response: HttpResponseBase) -> None: ...
 def patch_vary_headers(response: HttpResponseBase, newheaders: Tuple[str]) -> None: ...
 def has_vary_header(response: HttpResponse, header_query: str) -> bool: ...
 def get_cache_key(
-    request: WSGIRequest, key_prefix: Optional[str] = ..., method: str = ..., cache: Optional[BaseCache] = ...
+    request: Union[ASGIRequest, HttpRequest, WSGIRequest],
+    key_prefix: Optional[str] = ...,
+    method: str = ...,
+    cache: Optional[BaseCache] = ...
 ) -> Optional[str]: ...
 def learn_cache_key(
     request: WSGIRequest,
