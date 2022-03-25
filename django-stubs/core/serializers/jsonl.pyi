57f24b7fdb2ebd1fbd4cfc80f8e586637ea8036b
sterliakov <terlya.stas@gmail.com>
2022-03-25 21:37:20 +0300

    Improve typing of serializers.

--- django-stubs/core/serializers/jsonl.pyi
+++ django-stubs/core/serializers/jsonl.pyi
@@ -1,4 +1,3 @@
-import json
 from typing import Any, Dict, IO, Iterator, Union
 
 from django.core.serializers.python import Serializer as PythonSerializer
@@ -8,11 +7,3 @@ class Serializer(PythonSerializer):
     json_kwargs: Dict[str, Any]
 
 def Deserializer(stream_or_string: Union[IO[bytes], IO[str], bytes, str], **options: Any) -> Iterator[DeserializedObject]: ...
-
-class DjangoJSONEncoder(json.JSONEncoder):
-    allow_nan: bool
-    check_circular: bool
-    ensure_ascii: bool
-    indent: int
-    skipkeys: bool
-    sort_keys: bool
