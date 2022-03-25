07737126e67876a372dad9267be9f473670fb74b
sterliakov <terlya.stas@gmail.com>
2022-03-25 18:36:10 +0300

    Make all checks return Sequence[CheckMessage] or subclass to be covariant.

--- django-stubs/core/checks/async_checks.pyi
+++ django-stubs/core/checks/async_checks.pyi
@@ -1,8 +1,9 @@
-from typing import Any, List, Optional, Sequence
+from typing import Any, Optional, Sequence
 
 from django.apps.config import AppConfig
-from django.core.checks.messages import CheckMessage
+from django.core.checks.messages import Error
 
-E001: Any
+E001: Error
 
-def check_async_unsafe(app_configs: Optional[Sequence[AppConfig]] = ..., **kwargs: Any) -> List[CheckMessage]: ...
+
+def check_async_unsafe(app_configs: Optional[Sequence[AppConfig]], **kwargs: Any) -> Sequence[Error]: ...
--- django-stubs/core/checks/security/sessions.pyi
+++ django-stubs/core/checks/security/sessions.pyi
@@ -1,19 +1,19 @@
-from typing import Any, List, Optional, Sequence
+from typing import Any, Optional, Sequence
 
 from django.apps.config import AppConfig
 from django.core.checks.messages import Warning
 
-def add_session_cookie_message(message: Any): ...
+def add_session_cookie_message(message: str) -> str: ...
 
-W010: Any
-W011: Any
-W012: Any
+W010: Warning
+W011: Warning
+W012: Warning
 
-def add_httponly_message(message: Any): ...
+def add_httponly_message(message: str) -> str: ...
 
-W013: Any
-W014: Any
-W015: Any
+W013: Warning
+W014: Warning
+W015: Warning
 
-def check_session_cookie_secure(app_configs: Optional[Sequence[AppConfig]] = ..., **kwargs: Any) -> List[Warning]: ...
-def check_session_cookie_httponly(app_configs: Optional[Sequence[AppConfig]] = ..., **kwargs: Any) -> List[Warning]: ...
+def check_session_cookie_secure(app_configs: Optional[Sequence[AppConfig]], **kwargs: Any) -> Sequence[Warning]: ...
+def check_session_cookie_httponly(app_configs: Optional[Sequence[AppConfig]], **kwargs: Any) -> Sequence[Warning]: ...
