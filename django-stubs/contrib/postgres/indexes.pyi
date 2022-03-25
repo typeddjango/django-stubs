afef9d6bad29ae8cd21c769281e5f6a43034a81c
sterliakov <terlya.stas@gmail.com>
2022-03-25 22:27:03 +0300

    Actualize contrib.postgres

--- django-stubs/contrib/postgres/indexes.pyi
+++ django-stubs/contrib/postgres/indexes.pyi
@@ -1,10 +1,34 @@
-from typing import Optional, Sequence, Union
+from typing import Any, List, Optional, Sequence, Tuple, Type, Union
 
-from django.db.models import Func, Index
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+from django.db.backends.ddl_references import Statement
+from django.db.models import Func, Index, Model
 from django.db.models.expressions import BaseExpression, Combinable
 from django.db.models.query_utils import Q
+from django.utils.datastructures import _ListOrTuple
 
-class PostgresIndex(Index): ...
+class PostgresIndex(Index):
+    @property
+    def max_name_length(self) -> int: ...  # type: ignore
+    def create_sql(
+        self, model: Type[Model], schema_editor: BaseDatabaseSchemaEditor, using: str = ..., **kwargs: Any
+    ) -> Statement: ...
+    def check_supported(self, schema_editor: BaseDatabaseSchemaEditor) -> None:...
+    def get_with_params(self) -> Sequence[str]: ...
+
+class BloomIndex(PostgresIndex):
+    def __init__(
+        self,
+        *expressions: Union[BaseExpression, Combinable, str],
+        length: Optional[int] = ...,
+        columns: _ListOrTuple[int] = ...,
+        fields: Sequence[str] = ...,
+        name: Optional[str] = ...,
+        db_tablespace: Optional[str] = ...,
+        opclasses: Sequence[str] = ...,
+        condition: Optional[Q] = ...,
+        include: Optional[Sequence[str]] = ...,
+    ) -> None: ...
 
 class BrinIndex(PostgresIndex):
     def __init__(
--- django-stubs/contrib/postgres/search.pyi
+++ django-stubs/contrib/postgres/search.pyi
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional, TypeVar, Union
 
-from django.db.models import Field
-from django.db.models.expressions import Combinable, CombinedExpression, Func, Value, _OutputField
+from django.db.models import Field, Expression
+from django.db.models.expressions import Combinable, CombinedExpression, Func, Value
 from django.db.models.lookups import Lookup
 
 _Expression = Union[str, Combinable, "SearchQueryCombinable"]
@@ -10,17 +10,31 @@ class SearchVectorExact(Lookup): ...
 class SearchVectorField(Field): ...
 class SearchQueryField(Field): ...
 
+class SearchConfig(Expression):
+    config: Optional[_Expression] = ...
+    def __init__(self, config: _Expression) -> None: ...
+    @classmethod
+    def from_parameter(cls, config: Optional[_Expression]) -> SearchConfig: ...
+
 class SearchVectorCombinable:
     ADD: str = ...
 
 class SearchVector(SearchVectorCombinable, Func):
-    config: Optional[Any] = ...
-    def __init__(self, *expressions: _Expression, **extra: Any): ...
+    config: Optional[_Expression] = ...
+    function: str = ...
+    arg_joiner: str = ...
+    output_field: Field
+    def __init__(self, *expressions: _Expression, config: Optional[_Expression] = ..., weight: Optional[Any] = ...) -> None: ...
 
 class CombinedSearchVector(SearchVectorCombinable, CombinedExpression):
     def __init__(
-        self, lhs, connector, rhs, config: Optional[_Expression] = ..., output_field: Optional[_OutputField] = ...
-    ): ...
+        self,
+        lhs: Combinable,
+        connector: str,
+        rhs: Combinable,
+        config: Optional[_Expression],
+        output_field: Optional[Field] = None
+    ) -> None: ...
 
 _T = TypeVar("_T", bound="SearchQueryCombinable")
 
@@ -32,12 +46,12 @@ class SearchQueryCombinable:
     def __and__(self: _T, other: SearchQueryCombinable) -> _T: ...
     def __rand__(self: _T, other: SearchQueryCombinable) -> _T: ...
 
-class SearchQuery(SearchQueryCombinable, Value):  # type: ignore
+class SearchQuery(SearchQueryCombinable, Func):  # type: ignore
     SEARCH_TYPES: Dict[str, str] = ...
     def __init__(
         self,
-        value: str,
-        output_field: Optional[_OutputField] = ...,
+        value: _Expression,
+        output_field: Optional[Field] = ...,
         *,
         config: Optional[_Expression] = ...,
         invert: bool = ...,
@@ -47,16 +61,51 @@ class SearchQuery(SearchQueryCombinable, Value):  # type: ignore
 
 class CombinedSearchQuery(SearchQueryCombinable, CombinedExpression):  # type: ignore
     def __init__(
-        self, lhs, connector, rhs, config: Optional[_Expression] = ..., output_field: Optional[_OutputField] = ...
+        self,
+        lhs: Combinable,
+        connector: str,
+        rhs: Combinable,
+        config: Optional[_Expression],
+        output_field: Optional[Field] = None
     ) -> None: ...
 
 class SearchRank(Func):
     def __init__(
-        self, vector: Union[SearchVector, _Expression], query: Union[SearchQuery, _Expression], **extra: Any
+        self,
+        vector: Union[SearchVector, _Expression],
+        query: Union[SearchQuery, _Expression],
+        weights: Optional[Any] = ...,
+        normalization: Optional[Any] = ...,
+        cover_density: bool = ...,
+    ) -> None: ...
+
+class SearchHeadline(Func):
+    function: str = ...
+    template: str = ...
+    output_field: Field = ...
+
+    def __init__(
+        self,
+        expression: _Expression,
+        query: _Expression,
+        *,
+        config: Optional[_Expression] = ...,
+        start_sel: Optional[Any] = ...,
+        stop_sel: Optional[Any] = ...,
+        max_words: Optional[int] = ...,
+        min_words: Optional[int] = ...,
+        short_word: Optional[str] = ...,
+        highlight_all: Optional[bool] = ...,
+        max_fragments: Optional[int] = ...,
+        fragment_delimiter: Optional[str] = ...,
     ) -> None: ...
 
 class TrigramBase(Func):
-    def __init__(self, expression: _Expression, string, **extra: Any) -> None: ...
+    def __init__(self, expression: _Expression, string: str, **extra: Any) -> None: ...
 
-class TrigramSimilarity(TrigramBase): ...
-class TrigramDistance(TrigramBase): ...
+class TrigramSimilarity(TrigramBase):
+    function: str
+
+class TrigramDistance(TrigramBase):
+    function: str
+    arg_joiner: str
