aa5f114575d7cba7da3a69fb22173351bf143527
henribru <6639509+henribru@users.noreply.github.com>
2022-03-26 12:40:24 +0100

    Remove Python 3.6 trove classifier (#887)

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
