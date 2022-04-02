4c92fff2a8f178eeab17626ce1db663d2d2afa47
sterliakov <terlya.stas@gmail.com>
2022-04-03 01:51:54 +0300

    Fix #834.

--- django-stubs/utils/encoding.pyi
+++ django-stubs/utils/encoding.pyi
@@ -1,6 +1,12 @@
 import datetime
+import sys
 from decimal import Decimal
 from typing import Any, TypeVar, Union, overload
+
+if sys.version_info < (3, 10):
+    from typing_extensions import TypeGuard
+else:
+    from typing import TypeGuard
 
 from django.utils.functional import Promise
 from typing_extensions import Literal
@@ -11,10 +17,15 @@ class DjangoUnicodeDecodeError(UnicodeDecodeError):
 
 _P = TypeVar("_P", bound=Promise)
 _S = TypeVar("_S", bound=str)
+_B = TypeVar("_B", bound=bytes)
 _PT = TypeVar("_PT", None, int, float, Decimal, datetime.datetime, datetime.date, datetime.time)
 
 @overload
 def smart_str(s: _P, encoding: str = ..., strings_only: bool = ..., errors: str = ...) -> _P: ...
+@overload
+def smart_str(s: _S, encoding: str = ..., *, errors: str = ...) -> _S: ...
+@overload
+def smart_str(s: Any, encoding: str = ..., *, errors: str = ...) -> str: ...
 @overload
 def smart_str(s: _PT, encoding: str = ..., strings_only: Literal[True] = ..., errors: str = ...) -> _PT: ...
 @overload
@@ -24,7 +35,11 @@ def smart_str(s: Any, encoding: str = ..., strings_only: bool = ..., errors: str
 
 smart_text = smart_str  # Deprecated
 
-def is_protected_type(obj: Any) -> bool: ...
+def is_protected_type(obj: Any) -> TypeGuard[_PT]: ...
+@overload
+def force_str(s: _S, encoding: str = ..., *, errors: str = ...) -> _S: ...
+@overload
+def force_str(s: Any, encoding: str = ..., *, errors: str = ...) -> str: ...
 @overload
 def force_str(s: _PT, encoding: str = ..., strings_only: Literal[True] = ..., errors: str = ...) -> _PT: ...
 @overload
@@ -37,9 +52,17 @@ force_text = force_str  # Deprecated
 @overload
 def smart_bytes(s: _P, encoding: str = ..., strings_only: bool = ..., errors: str = ...) -> _P: ...
 @overload
+def smart_bytes(s: _B, encoding: Literal["utf-8"] = ..., *, errors: str = ...) -> _B: ...
+@overload
+def smart_bytes(s: Any, encoding: str = ..., *, errors: str = ...) -> bytes: ...
+@overload
 def smart_bytes(s: _PT, encoding: str = ..., strings_only: Literal[True] = ..., errors: str = ...) -> _PT: ...
 @overload
 def smart_bytes(s: Any, encoding: str = ..., strings_only: bool = ..., errors: str = ...) -> bytes: ...
+@overload
+def force_bytes(s: _B, encoding: Literal["utf-8"] = ..., *, errors: str = ...) -> _B: ...
+@overload
+def force_bytes(s: Any, encoding: str = ..., *, errors: str = ...) -> bytes: ...
 @overload
 def force_bytes(s: _PT, encoding: str = ..., strings_only: Literal[True] = ..., errors: str = ...) -> _PT: ...
 @overload
