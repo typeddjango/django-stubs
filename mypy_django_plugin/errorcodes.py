dab26d0e0eea3d60da08ab8d536e12d96165ec27
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2022-06-13 17:49:59 +0200

    Add fallback related manager in final iteration of AddRelatedManagers
    If a django model has a Manager class that cannot be resolved statically
    (if it is generated in a way where we cannot import it, like `objects =
    my_manager_factory()`), we fallback to the default related manager, so
    you at least get a base level of working type checking.

--- mypy_django_plugin/errorcodes.py
+++ mypy_django_plugin/errorcodes.py
@@ -1,3 +1,4 @@
 from mypy.errorcodes import ErrorCode
 
 MANAGER_UNTYPED = ErrorCode("django-manager", "Untyped manager disallowed", "Django")
+MANAGER_MISSING = ErrorCode("django-manager-missing", "Couldn't resolve related manager for model", "Django")
