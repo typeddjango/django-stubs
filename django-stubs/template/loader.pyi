926661ab4ae50ad4796ebe9f15a269f7a18a45c4
David Smith <39445562+smithdc1@users.noreply.github.com>
2022-06-26 22:48:10 +0100

    Improved type of template.loader.get_template() (#1011)
    * Improved type of template.loader.get_template()
    
    * Removed Optional from return type.
    
    * Edits.

--- django-stubs/template/loader.pyi
+++ django-stubs/template/loader.pyi
@@ -4,8 +4,9 @@ from django.http.request import HttpRequest
 from django.template.exceptions import TemplateDoesNotExist as TemplateDoesNotExist  # noqa: F401
 
 from . import engines as engines  # noqa: F401
+from .backends.base import _EngineTemplate
 
-def get_template(template_name: str, using: Optional[str] = ...) -> Any: ...
+def get_template(template_name: str, using: Optional[str] = ...) -> _EngineTemplate: ...
 def select_template(template_name_list: Union[Sequence[str], str], using: Optional[str] = ...) -> Any: ...
 def render_to_string(
     template_name: Union[Sequence[str], str],
