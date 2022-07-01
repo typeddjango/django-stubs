b5cfdb1ac3b6df7e02c052fc8be4e52f5a179a5b
Zixuan James Li <p359101898@gmail.com>
2022-07-01 10:44:13 -0400

    Fix user_passes_test to use AUTH_USER_MODEL.
    According to the documentation, `test_func` is a callable that takes a
    `User` (possibly anonymous).
    
    Relevant documentation:
    https://docs.djangoproject.com/en/4.0/topics/auth/default/#django.contrib.auth.decorators.user_passes_test
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- django-stubs/contrib/auth/decorators.pyi
+++ django-stubs/contrib/auth/decorators.pyi
@@ -1,13 +1,15 @@
 from typing import Callable, Iterable, Optional, TypeVar, Union, overload
 
 from django.contrib.auth import REDIRECT_FIELD_NAME as REDIRECT_FIELD_NAME  # noqa: F401
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
 from django.http.response import HttpResponseBase
 
 _VIEW = TypeVar("_VIEW", bound=Callable[..., HttpResponseBase])
 
 def user_passes_test(
-    test_func: Callable[[AbstractUser], bool], login_url: Optional[str] = ..., redirect_field_name: str = ...
+    test_func: Callable[[Union[AbstractBaseUser, AnonymousUser]], bool],
+    login_url: Optional[str] = ...,
+    redirect_field_name: str = ...,
 ) -> Callable[[_VIEW], _VIEW]: ...
 
 # There are two ways of calling @login_required: @with(arguments) and @bare
