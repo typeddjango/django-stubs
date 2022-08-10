3ac34f110800e555a380b80ea2fb8758c19ebea2
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-08-10 14:16:54 +0100

    Fix base_field type for SimpleArrayField (#1097)
    * Fix base_field type for SimpleArrayField
    
    * Add test which fails without this change

--- django-stubs/contrib/postgres/forms/array.pyi
+++ django-stubs/contrib/postgres/forms/array.pyi
@@ -13,13 +13,13 @@ from ..utils import prefix_validation_error as prefix_validation_error
 
 class SimpleArrayField(forms.CharField):
     default_error_messages: _ErrorMessagesT = ...
-    base_field: Type[forms.Field]
+    base_field: forms.Field
     delimiter: str
     min_length: Optional[int]
     max_length: Optional[int]
     def __init__(
         self,
-        base_field: Type[forms.Field],
+        base_field: forms.Field,
         *,
         delimiter: str = ...,
         max_length: Optional[int] = ...,
@@ -51,11 +51,11 @@ class SplitArrayWidget(forms.Widget):
 
 class SplitArrayField(forms.Field):
     default_error_messages: _ErrorMessagesT = ...
-    base_field: Type[forms.Field]
+    base_field: forms.Field
     size: int
     remove_trailing_nulls: bool
     def __init__(
-        self, base_field: Type[forms.Field], size: int, *, remove_trailing_nulls: bool = ..., **kwargs: Any
+        self, base_field: forms.Field, size: int, *, remove_trailing_nulls: bool = ..., **kwargs: Any
     ) -> None: ...
     def to_python(self, value: Any) -> Sequence[Any]: ...
     def clean(self, value: Any) -> Sequence[Any]: ...
