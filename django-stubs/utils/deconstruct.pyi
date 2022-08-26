732ef95caba298b63fb4403fa467b50ad6df13cb
Nikita Sobolev <mail@sobolevn.me>
2022-08-27 01:32:45 +0300

    Black

--- django-stubs/utils/deconstruct.pyi
+++ django-stubs/utils/deconstruct.pyi
@@ -1,14 +1,10 @@
 from typing import Any, Callable, Optional, TypeVar, overload
 
-_T = TypeVar('_T')
-_TCallable = TypeVar('_TCallable', bound=Callable[..., Any])
+_T = TypeVar("_T")
+_TCallable = TypeVar("_TCallable", bound=Callable[..., Any])
 
 
 @overload
 def deconstructible(_type: type[_T]) -> type[_T]: ...
-
-
 @overload
-def deconstructible(
-    *, path: Optional[str] = ...
-) -> Callable[[_TCallable], _TCallable]: ...
+def deconstructible(*, path: Optional[str] = ...) -> Callable[[_TCallable], _TCallable]: ...
