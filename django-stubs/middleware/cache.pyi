c0414c7b025b77caf9f72dbaf9e8471e11aad085
PIG208 <39874143+PIG208@users.noreply.github.com>
2022-09-03 16:53:14 -0400

    Reflect the deprecation of get_response being None. (#1086)
    * Reflect the deprecation of get_response being None.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>
    
    * Type get_response with a callback protocol.
    
    Otherwise, calling `self.get_response(request)` in a subclass of
    `MiddlewareMixin` runs into `Invalid self argument` error.
    
    This is a workaround for https://github.com/python/mypy/issues/5485.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- django-stubs/middleware/cache.pyi
+++ django-stubs/middleware/cache.pyi
@@ -27,7 +27,7 @@ class CacheMiddleware(UpdateCacheMiddleware, FetchFromCacheMiddleware):
     cache: BaseCache = ...
     def __init__(
         self,
-        get_response: Optional[Callable] = ...,
+        get_response: Callable = ...,
         cache_timeout: Optional[float] = ...,
         page_timeout: Optional[float] = ...,
         **kwargs: Any
