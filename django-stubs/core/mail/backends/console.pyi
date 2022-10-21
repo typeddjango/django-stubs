fb9ad1c7fe20532bcdb301f9f64f2691cb0e0097
James Owen <james.owen@octoenergy.com>
2022-10-21 16:42:21 +0100

    Add hints for instance variables in console.EmailBackend (#1200)

--- django-stubs/core/mail/backends/console.pyi
+++ django-stubs/core/mail/backends/console.pyi
@@ -1,3 +1,8 @@
+import threading
+from typing import TextIO
+
 from django.core.mail.backends.base import BaseEmailBackend
 
-class EmailBackend(BaseEmailBackend): ...
+class EmailBackend(BaseEmailBackend):
+    stream: TextIO = ...
+    _lock: threading.RLock = ...
