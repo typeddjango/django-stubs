1d78b8ffacd1be926fbad0a45431db0d31b4fd39
Adam Johnson <me@adamj.eu>
2022-11-03 13:18:05 +0000

    Add SQLite backend is_in_memory_db() functions (#1218)
    * Add SQLite backend is_in_memory_db() functions
    
    * Update django-stubs/db/backends/sqlite3/creation.pyi
    
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>

--- django-stubs/db/backends/sqlite3/creation.pyi
+++ django-stubs/db/backends/sqlite3/creation.pyi
@@ -1,5 +1,11 @@
+from os import PathLike
+from typing import Union
+
 from django.db.backends.base.creation import BaseDatabaseCreation
 from django.db.backends.sqlite3.base import DatabaseWrapper
 
 class DatabaseCreation(BaseDatabaseCreation):
     connection: DatabaseWrapper
+
+    @staticmethod
+    def is_in_memory_db(database_name: Union[str, PathLike[str]]) -> bool: ...
