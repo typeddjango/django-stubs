c5655ac9c82a22272af689276c497e707d3ddb81
Nikita Sobolev <mail@sobolevn.me>
2022-11-03 16:00:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def find_stub_files(name: str) -> List[str]:
     result = []
     for root, _dirs, files in os.walk(name):
         for file in files:
-            if file.endswith(".pyi"):
+            if file.endswith('.pyi'):
                 if os.path.sep in root:
                     sub_root = root.split(os.path.sep, 1)[-1]
                     file = os.path.join(sub_root, file)
