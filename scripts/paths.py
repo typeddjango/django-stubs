ce1e209429082fb881390b3eabf5cfad2bfa0fb5
Ben Spaulding <ben@spaulding.im>
2022-11-25 22:20:53 -0700

    Build stubs only for Django source code
    The namespace tests were causing errors and are not needed for stub
    generation anyway.
    
    ```
    Critical error during semantic analysis: django-source/tests/apps/namespace_package_base/nsapp/apps.py: error: Duplicate module named "apps" (also at "tests/apps/__init__.py")
    ```

--- scripts/paths.py
+++ scripts/paths.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 PROJECT_DIRECTORY = Path(__file__).parent.parent
 DJANGO_SOURCE_DIRECTORY = PROJECT_DIRECTORY / "django-source"  # type: Path
+STUBGEN_OUTPUT_DIRECTORY = PROJECT_DIRECTORY / "stubgen"  # type: Path
--- scripts/stubgen-django.py
+++ scripts/stubgen-django.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from mypy.stubgen import generate_stubs, parse_options
 
 from scripts.git_helpers import checkout_django_branch
-from scripts.paths import DJANGO_SOURCE_DIRECTORY
+from scripts.paths import DJANGO_SOURCE_DIRECTORY, STUBGEN_OUTPUT_DIRECTORY
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -11,5 +11,6 @@ if __name__ == "__main__":
     parser.add_argument("--commit_sha", required=False)
     args = parser.parse_args()
     checkout_django_branch(args.django_version, args.commit_sha)
-    stubgen_options = parse_options([f"{DJANGO_SOURCE_DIRECTORY}", "-o=stubgen"])
+    django_root = DJANGO_SOURCE_DIRECTORY / "django"
+    stubgen_options = parse_options([f"{django_root}", f"-o={STUBGEN_OUTPUT_DIRECTORY}"])
     generate_stubs(stubgen_options)
