typeddjango/django-stubs

commit b81b1bf3868eaab9fecebdd56ed817ffcb07fad8

Introduce `flake8-pyi` in `pre-commit` checks (#1253)
* only allow redirect_chain attribute in response when client sets the follow flag

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* compat for py3.7

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* compat for py3.7, fixed

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* address review comments, extract follow flag checks in a separate test case

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* configure flake8-pyi plugin in pre-commit

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* use pep 604 union types where possible

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* fix Y015

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* use PEP 585 container types where possible, replace obsolete typing types with collections.abc

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* allow isort to reorder non-top imports

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* finish PEP 585 changes, use PEP 613 where possible

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* fix remaining flake8-pyi warnings, silence warnings in question

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* fix Y023

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

* fix Y029

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

django-stubs/utils/functional.pyi
@@ -1,30 +1,32 @@
+from collections.abc import Callable, Sequence
 from functools import wraps as wraps  # noqa: F401
-from typing import Any, Callable, Generic, List, Optional, Sequence, Tuple, Type, TypeVar, Union, overload
+from typing import Any, Generic, Protocol, TypeVar, overload
 
+from _typeshed import Self
 from django.db.models.base import Model
-from typing_extensions import Protocol, SupportsIndex
+from typing_extensions import SupportsIndex, TypeAlias
 
 _T = TypeVar("_T")
 
 class cached_property(Generic[_T]):
-    func: Callable[..., _T] = ...
-    name: str = ...
+    func: Callable[..., _T]
+    name: str
     def __init__(self, func: Callable[..., _T], name: str = ...) -> None: ...
     @overload
-    def __get__(self, instance: None, cls: Type[Any] = ...) -> "cached_property[_T]": ...
+    def __get__(self: Self, instance: None, cls: type[Any] = ...) -> Self: ...
     @overload
-    def __get__(self, instance: object, cls: Type[Any] = ...) -> _T: ...
+    def __get__(self, instance: object, cls: type[Any] = ...) -> _T: ...
 
 # Promise is only subclassed by a proxy class defined in the lazy function
 # so it makes sense for it to have all the methods available in that proxy class
 class Promise:
     def __init__(self, args: Any, kw: Any) -> None: ...
-    def __reduce__(self) -> Tuple[Any, Tuple[Any]]: ...
+    def __reduce__(self) -> tuple[Any, tuple[Any]]: ...
     def __lt__(self, other: Any) -> bool: ...
     def __mod__(self, rhs: Any) -> Any: ...
     def __add__(self, other: Any) -> Any: ...
     def __radd__(self, other: Any) -> Any: ...
-    def __deepcopy__(self: _T, memo: Any) -> _T: ...
+    def __deepcopy__(self: Self, memo: Any) -> Self: ...
 
 class _StrPromise(Promise, Sequence[str]):
     def __add__(self, __s: str) -> str: ...
@@ -43,7 +45,7 @@ class _StrPromise(Promise, Sequence[str]):
     # Mypy requires this for the attribute hook to take effect
     def __getattribute__(self, __name: str) -> Any: ...
 
-_StrOrPromise = Union[str, _StrPromise]
+_StrOrPromise: TypeAlias = str | _StrPromise  # noqa: Y047
 _C = TypeVar("_C", bound=Callable)
 
 def lazy(func: _C, *resultclasses: Any) -> _C: ...
@@ -57,22 +59,22 @@ def new_method_proxy(func: Callable[..., _T]) -> Callable[..., _T]: ...
 
 class LazyObject:
     def __init__(self) -> None: ...
-    __getattr__: Callable = ...
+    __getattr__: Callable
     def __setattr__(self, name: str, value: Any) -> None: ...
     def __delattr__(self, name: str) -> None: ...
-    def __reduce__(self) -> Tuple[Callable, Tuple[Model]]: ...
+    def __reduce__(self) -> tuple[Callable, tuple[Model]]: ...
     def __copy__(self) -> LazyObject: ...
-    __bytes__: Callable = ...
-    __bool__: Callable = ...
-    __dir__: Callable = ...
-    __ne__: Callable = ...
-    __hash__: Callable = ...
-    __getitem__: Callable = ...
-    __setitem__: Callable = ...
-    __delitem__: Callable = ...
-    __iter__: Callable = ...
-    __len__: Callable = ...
-    __contains__: Callable = ...
+    __bytes__: Callable
+    __bool__: Callable
+    __dir__: Callable
+    __ne__: Callable
+    __hash__: Callable
+    __getitem__: Callable
+    __setitem__: Callable
+    __delitem__: Callable
+    __iter__: Callable
+    __len__: Callable
+    __contains__: Callable
 
 def unpickle_lazyobject(wrapped: Model) -> Model: ...
 
@@ -83,17 +85,16 @@ class SimpleLazyObject(LazyObject):
 _PartitionMember = TypeVar("_PartitionMember")
 
 def partition(
-    predicate: Callable[[_PartitionMember], Union[int, bool]], values: List[_PartitionMember]
-) -> Tuple[List[_PartitionMember], List[_PartitionMember]]: ...
+    predicate: Callable[[_PartitionMember], int | bool], values: list[_PartitionMember]
+) -> tuple[list[_PartitionMember], list[_PartitionMember]]: ...
 
 _Get = TypeVar("_Get", covariant=True)
-_Self = TypeVar("_Self")
 
 class classproperty(Generic[_Get]):
-    fget: Optional[Callable[[_Self], _Get]] = ...
-    def __init__(self, method: Optional[Callable[[_Self], _Get]] = ...) -> None: ...
-    def __get__(self, instance: Optional[_Self], cls: Type[_Self] = ...) -> _Get: ...
-    def getter(self, method: Callable[[_Self], _Get]) -> classproperty[_Get]: ...
+    fget: Callable[[Self], _Get] | None
+    def __init__(self: Self, method: Callable[[Self], _Get] | None = ...) -> None: ...
+    def __get__(self: Self, instance: Self | None, cls: type[Self] = ...) -> _Get: ...
+    def getter(self: Self, method: Callable[[Self], _Get]) -> classproperty[_Get]: ...
 
 class _Getter(Protocol[_Get]):
     """Type fake to declare some read-only properties (until `property` builtin is generic)
@@ -103,6 +104,6 @@ class _Getter(Protocol[_Get]):
     """
 
     @overload
-    def __get__(self: _Self, __instance: None, __typeobj: Optional[Type[Any]]) -> _Self: ...
+    def __get__(self: Self, __instance: None, __typeobj: type[Any] | None) -> Self: ...
     @overload
-    def __get__(self, __instance: Any, __typeobj: Optional[Type[Any]]) -> _Get: ...
+    def __get__(self, __instance: Any, __typeobj: type[Any] | None) -> _Get: ...

commit 16fde01583fafd496c8f3c7587a7630925e191de

Fix @classproperty decorator (#1287)
Removed unnecessary `Self` hints on classproperty methods.

The removal of `self: Self` fixes #1285. Regressed in #1253.

django-stubs/utils/functional.pyi
@@ -91,10 +91,10 @@ def partition(
 _Get = TypeVar("_Get", covariant=True)
 
 class classproperty(Generic[_Get]):
-    fget: Callable[[Self], _Get] | None
-    def __init__(self: Self, method: Callable[[Self], _Get] | None = ...) -> None: ...
-    def __get__(self: Self, instance: Self | None, cls: type[Self] = ...) -> _Get: ...
-    def getter(self: Self, method: Callable[[Self], _Get]) -> classproperty[_Get]: ...
+    fget: Callable[[Any], _Get] | None
+    def __init__(self, method: Callable[[Any], _Get] | None = ...) -> None: ...
+    def __get__(self, instance: Any | None, cls: type[Any] = ...) -> _Get: ...
+    def getter(self, method: Callable[[Any], _Get]) -> classproperty[_Get]: ...
 
 class _Getter(Protocol[_Get]):
     """Type fake to declare some read-only properties (until `property` builtin is generic)

tests/typecheck/utils/test_functional.yml
@@ -49,3 +49,21 @@
 
       foo(s)                         # E: Argument 1 to "foo" has incompatible type "_StrPromise"; expected "str"
       bar(s)
+
+-   case: classproperty_usage
+    main: |
+      from typing import Any
+      from django.utils.functional import classproperty
+
+      class Foo:
+          @classproperty
+          def attr(cls: Any) -> str: ...
+
+          reveal_type(attr)      # N: Revealed type is "django.utils.functional.classproperty[builtins.str]"
+          reveal_type(attr.getter)  # N: Revealed type is "def (method: def (Any) -> builtins.str) -> django.utils.functional.classproperty[builtins.str]"
+
+      reveal_type(Foo.attr)      # N: Revealed type is "builtins.str"
+
+      class Bar(Foo):
+          def method(self) -> None:
+              reveal_type(self.attr)  # N: Revealed type is "builtins.str"