f34a626b3bcb61ef1b1a6afd18971a9af8a37e6b
Matthias Schoettle <git@mattsch.com>
2022-12-31 02:12:34 -0500

    change render_to_string context type (#1306)

--- django-stubs/template/loader.pyi
+++ django-stubs/template/loader.pyi
@@ -7,13 +7,12 @@ from django.utils.safestring import SafeString
 
 from . import engines as engines  # noqa: F401
 from .backends.base import _EngineTemplate
-from .context import _ContextKeys
 
 def get_template(template_name: str, using: str | None = ...) -> _EngineTemplate: ...
 def select_template(template_name_list: Sequence[str] | str, using: str | None = ...) -> Any: ...
 def render_to_string(
     template_name: Sequence[str] | str,
-    context: Mapping[_ContextKeys, Any] | None = ...,
+    context: dict[str, Any] | None = ...,
     request: HttpRequest | None = ...,
     using: str | None = ...,
 ) -> SafeString: ...
