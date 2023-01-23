aa9dea3310f40c73c89da5bf39a786500b0aabfd
Christian Bundy <christian@truework.com>
2023-01-23 08:46:06 -0800

    Fix issues

--- django-stubs/contrib/admin/decorators.pyi
+++ django-stubs/contrib/admin/decorators.pyi
@@ -1,5 +1,5 @@
 from collections.abc import Callable, Sequence
-from typing import Any, Optional, TypeVar, Union, overload  # noqa: Y037
+from typing import Any, TypeVar, Union, overload  # noqa: Y037
 
 from django.contrib.admin import ModelAdmin
 from django.contrib.admin.sites import AdminSite
@@ -17,23 +17,21 @@ _QuerySet = TypeVar("_QuerySet", bound=QuerySet)
 # This is deliberately different from _DisplayT defined in contrib.admin.options
 _DisplayCallable: TypeAlias = Union[Callable[[_ModelAdmin, _Model], Any], Callable[[_Model], Any]]  # noqa: Y037
 _DisplayCallableT = TypeVar("_DisplayCallableT", bound=_DisplayCallable)
-_ActionReturn = TypeVar("_ActionReturn", bound=Optional[HttpResponseBase])
+_ActionCallable: TypeAlias = Callable[[_ModelAdmin, _Request, _QuerySet], HttpResponseBase | None]  # noqa: Y037
+_ActionCallableT = TypeVar("_ActionCallableT", bound=_ActionCallable)
 
 @overload
 def action(
-    function: Callable[[_ModelAdmin, _Request, _QuerySet], None],
+    function: _ActionCallableT,
     permissions: Sequence[str] | None = ...,
     description: _StrOrPromise | None = ...,
-) -> Callable[[_ModelAdmin, _Request, _QuerySet], Optional[HttpResponseBase]]: ...
+) -> _ActionCallableT: ...
 @overload
 def action(
     *,
     permissions: Sequence[str] | None = ...,
     description: _StrOrPromise | None = ...,
-) -> Callable[
-    [Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn]],
-    Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn],
-]: ...
+) -> Callable[[_ActionCallableT], _ActionCallableT,]: ...
 @overload
 def display(
     function: _DisplayCallableT,
