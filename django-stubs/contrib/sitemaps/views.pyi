9f682c785b4faa90e6df14d98070a2dec436a0fa
Christian Bundy <christian@truework.com>
2023-01-23 23:14:33 -0800

    Fix Sitemap

--- django-stubs/contrib/sitemaps/views.pyi
+++ django-stubs/contrib/sitemaps/views.pyi
@@ -10,14 +10,14 @@ _C = TypeVar("_C", bound=Callable[..., Any])
 def x_robots_tag(func: _C) -> _C: ...
 def index(
     request: HttpRequest,
-    sitemaps: dict[str, type[Sitemap] | Sitemap],
+    sitemaps: dict[str, type[Sitemap[Any]] | Sitemap[Any]],
     template_name: str = ...,
     content_type: str = ...,
     sitemap_url_name: str = ...,
 ) -> TemplateResponse: ...
 def sitemap(
     request: HttpRequest,
-    sitemaps: dict[str, type[Sitemap] | Sitemap],
+    sitemaps: dict[str, type[Sitemap[Any]] | Sitemap[Any]],
     section: str | None = ...,
     template_name: str = ...,
     content_type: str = ...,
