1b320cc7efdf4ae133acd904ba0c06880a6b540c
Marti Raudsepp <marti@juffo.org>
2023-01-25 15:20:14 +0200

    Typehint SpatialOperator.as_sql as well

--- django-stubs/contrib/gis/db/backends/utils.pyi
+++ django-stubs/contrib/gis/db/backends/utils.pyi
@@ -1,5 +1,8 @@
+from collections.abc import Mapping, Sequence
 from typing import Any
 
+from django.contrib.gis.db.models.lookups import GISLookup
+from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models.sql.compiler import _AsSqlType
 
 class SpatialOperator:
@@ -9,4 +12,10 @@ class SpatialOperator:
     def __init__(self, op: Any | None = ..., func: Any | None = ...) -> None: ...
     @property
     def default_template(self) -> Any: ...
-    def as_sql(self, connection: Any, lookup: Any, template_params: Any, sql_params: Any) -> _AsSqlType: ...
+    def as_sql(
+        self,
+        connection: BaseDatabaseWrapper,
+        lookup: GISLookup,
+        template_params: Mapping[str, Any],
+        sql_params: Sequence[Any],
+    ) -> _AsSqlType: ...
