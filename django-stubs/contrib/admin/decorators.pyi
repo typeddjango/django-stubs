832163dad64298f00efe4c4b1b210bd6f766f8b0
Christian Bundy <christian@truework.com>
2023-01-25 03:16:05 -0800

    Allow returning Response from admin @action callbacks (#1331)
    Problem: The Django docs say that you can "return an HttpResponse (or subclass) from your action", but the stubs currently say that actions can only return `None`.
    
    Solution: Update stubs to return `HttpResponse | None` instead.
    
    * Fix admin action return type
    * Use HttpResponseBase to support StreamingHttpResponse
    * Use `T | None` rather than `Optional[T]`
    * Add freestanding actions to test
    * Add negative test cases
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- django-stubs/contrib/admin/decorators.pyi
+++ django-stubs/contrib/admin/decorators.pyi
@@ -6,7 +6,7 @@ from django.contrib.admin.sites import AdminSite
 from django.db.models import Combinable, QuerySet
 from django.db.models.base import Model
 from django.db.models.expressions import BaseExpression
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseBase
 from django.utils.functional import _StrOrPromise
 from typing_extensions import TypeAlias
 
@@ -17,20 +17,22 @@ _QuerySet = TypeVar("_QuerySet", bound=QuerySet)
 # This is deliberately different from _DisplayT defined in contrib.admin.options
 _DisplayCallable: TypeAlias = Union[Callable[[_ModelAdmin, _Model], Any], Callable[[_Model], Any]]  # noqa: Y037
 _DisplayCallableT = TypeVar("_DisplayCallableT", bound=_DisplayCallable)
+_ActionReturn = TypeVar("_ActionReturn", bound=HttpResponseBase | None)
 
 @overload
 def action(
-    function: Callable[[_ModelAdmin, _Request, _QuerySet], None],
+    function: Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn],
     permissions: Sequence[str] | None = ...,
     description: _StrOrPromise | None = ...,
-) -> Callable[[_ModelAdmin, _Request, _QuerySet], None]: ...
+) -> Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn]: ...
 @overload
 def action(
     *,
     permissions: Sequence[str] | None = ...,
     description: _StrOrPromise | None = ...,
 ) -> Callable[
-    [Callable[[_ModelAdmin, _Request, _QuerySet], None]], Callable[[_ModelAdmin, _Request, _QuerySet], None]
+    [Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn]],
+    Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn],
 ]: ...
 @overload
 def display(
