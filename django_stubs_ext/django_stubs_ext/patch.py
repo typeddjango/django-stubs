b4f1c3c768fad342eeda2c4945eaad9c21b06e9d
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2023-02-03 08:09:19 +0000

    Add generic BaseConnectionHandler (#1349)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- django_stubs_ext/django_stubs_ext/patch.py
+++ django_stubs_ext/django_stubs_ext/patch.py
@@ -15,6 +15,7 @@ from django.db.models.manager import BaseManager
 from django.db.models.query import QuerySet
 from django.forms.formsets import BaseFormSet
 from django.forms.models import BaseModelForm, BaseModelFormSet
+from django.utils.connection import BaseConnectionHandler
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import DeletionMixin, FormMixin
 from django.views.generic.list import MultipleObjectMixin
@@ -64,6 +65,7 @@ _need_generic: List[MPGeneric[Any]] = [
     MPGeneric(Sitemap),
     MPGeneric(FileProxyMixin),
     MPGeneric(Lookup),
+    MPGeneric(BaseConnectionHandler),
     # These types do have native `__class_getitem__` method since django 3.1:
     MPGeneric(QuerySet, (3, 1)),
     MPGeneric(BaseManager, (3, 1)),
