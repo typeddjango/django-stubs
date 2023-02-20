d1a1c81f7c8d14d0aeed06e72e2ef5ca651f750a
Anvansh <32684077+RyanWalker277@users.noreply.github.com>
2023-02-20 07:16:12 +0530

    Fixed Collector.data and NestedObjects.data types (#1366)
    Specified dict[type[Model], set[Model] | list[Model]] type for Collecor.data attribute.

--- django-stubs/contrib/contenttypes/management/commands/remove_stale_contenttypes.pyi
+++ django-stubs/contrib/contenttypes/management/commands/remove_stale_contenttypes.pyi
@@ -1,12 +1,13 @@
 from typing import Any
 
 from django.core.management import BaseCommand
+from django.db.models.base import Model
 from django.db.models.deletion import Collector
 
 class Command(BaseCommand): ...
 
 class NoFastDeleteCollector(Collector):
-    data: dict[str, Any]
+    data: dict[type[Model], set[Model] | list[Model]]
     dependencies: dict[Any, Any]
     fast_deletes: list[Any]
     field_updates: dict[Any, Any]
