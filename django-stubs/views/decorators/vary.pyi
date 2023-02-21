d54f6a41b3e1f599e425ef3120743616a323ae0c
Ryanwalker277 <andianurag277@gmail.com>
2023-02-21 14:22:20 +0530

    Pre-commit changes

--- django-stubs/views/decorators/vary.pyi
+++ django-stubs/views/decorators/vary.pyi
@@ -1,7 +1,10 @@
 from collections.abc import Callable
 from typing import Any, TypeVar
 
+from typing_extensions import ParamSpec
+
+_P = ParamSpec("_P")
 _F = TypeVar("_F", bound=Callable[..., Any])
 
-def vary_on_headers(*headers: Any) -> Callable: ...
+def vary_on_headers(*headers: Callable[_P, _F]) -> Callable[_P, _F]: ...
 def vary_on_cookie(func: _F) -> _F: ...
