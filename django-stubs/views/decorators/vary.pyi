c6626e693ef0252a55a37113d817709aa92520ea
Ryanwalker277 <andianurag277@gmail.com>
2023-02-22 11:15:49 +0530

    Added sample definition using functools.wraps

--- django-stubs/views/decorators/vary.pyi
+++ django-stubs/views/decorators/vary.pyi
@@ -1,10 +1,17 @@
+import functools
 from collections.abc import Callable
-from typing import Any, TypeVar
+from typing import Any, TypeVar, cast
 
-from typing_extensions import ParamSpec
-
-_P = ParamSpec("_P")
 _F = TypeVar("_F", bound=Callable[..., Any])
 
-def vary_on_headers(*headers: Callable[_P, _F]) -> Callable[_P, _F]: ...
+def vary_on_headers(func: _F) -> _F: ...
+
+# def vary_on_headers(*headers: Any) -> Callable[[_F], _F]:
+#     def decorator(func: _F) -> _F:
+#         @functools.wraps(func)
+#         def wrapper(*args: Any, **kwargs: Any) -> Any:
+#             return func(*args, **kwargs)
+#         return cast(_F, wrapper)
+#     return decorator
+
 def vary_on_cookie(func: _F) -> _F: ...
