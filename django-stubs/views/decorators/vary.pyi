120c4cf3454f22357ee8a977a08b2f1301ad1a92
Ryanwalker277 <andianurag277@gmail.com>
2023-02-23 16:10:06 +0530

    Modified type-definition for vary_on_headers

--- django-stubs/views/decorators/vary.pyi
+++ django-stubs/views/decorators/vary.pyi
@@ -4,14 +4,5 @@ from typing import Any, TypeVar, cast
 
 _F = TypeVar("_F", bound=Callable[..., Any])
 
-def vary_on_headers(func: _F) -> _F: ...
-
-# def vary_on_headers(*headers: Any) -> Callable[[_F], _F]:
-#     def decorator(func: _F) -> _F:
-#         @functools.wraps(func)
-#         def wrapper(*args: Any, **kwargs: Any) -> Any:
-#             return func(*args, **kwargs)
-#         return cast(_F, wrapper)
-#     return decorator
-
+def vary_on_headers(*headers: str) -> Callable[[_F], _F]: ...
 def vary_on_cookie(func: _F) -> _F: ...
