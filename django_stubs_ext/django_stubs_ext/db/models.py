f1640aff6a03311c3efff4f0502cc80cf75b9c97
Marti Raudsepp <marti@juffo.org>
2023-04-07 14:49:07 +0300

    Move behind `if TYPE_CHECKING`

--- django_stubs_ext/django_stubs_ext/db/models.py
+++ django_stubs_ext/django_stubs_ext/db/models.py
@@ -1,51 +1,56 @@
-from typing import ClassVar, List, Sequence, Tuple, Union
+from typing import TYPE_CHECKING
 
-from django.db.models import BaseConstraint, Index
-from typing_extensions import Literal
+if TYPE_CHECKING:
+    from typing import ClassVar, List, Sequence, Tuple, Union
 
-from django_stubs_ext import StrOrPromise
+    from django.db.models import BaseConstraint, Index
+    from typing_extensions import Literal
 
+    from django_stubs_ext import StrOrPromise
 
-class BaseModelMeta:
-    """
-    Typed base class for Django Model `class Meta:` inner class.
+    class BaseModelMeta:
+        """
+        Typed base class for Django Model `class Meta:` inner class.
 
-    Most attributes are the same as `django.db.models.options.Options`. Options has some additional attributes and some
-    values are normalized by Django.
+        Most attributes are the same as `django.db.models.options.Options`. Options has some additional attributes and some
+        values are normalized by Django.
 
-    Usage::
+        Usage::
 
-        from django.db import models
-        from django_stubs_ext.db.models import BaseModelMeta
+            from django.db import models
+            from django_stubs_ext.db.models import BaseModelMeta
 
-        class MyModel(models.Model):
-            example = models.CharField(max_length=100)
+            class MyModel(models.Model):
+                example = models.CharField(max_length=100)
 
-            class Meta(BaseModelMeta):
-                ordering = ["example"]
-    """
+                class Meta(BaseModelMeta):
+                    ordering = ["example"]
+        """
 
-    abstract: ClassVar[bool]  # default: False
-    app_label: ClassVar[str]
-    base_manager_name: ClassVar[str]
-    db_table: ClassVar[str]
-    db_table_comment: ClassVar[str]
-    db_tablespace: ClassVar[str]
-    default_manager_name: ClassVar[str]
-    default_related_name: ClassVar[str]
-    get_latest_by: ClassVar[Union[str, Sequence[str]]]
-    managed: ClassVar[bool]  # default: True
-    order_with_respect_to: ClassVar[str]
-    ordering: ClassVar[Sequence[str]]
-    permissions: ClassVar[List[Tuple[str, str]]]
-    default_permissions: ClassVar[Sequence[str]]  # default: ("add", "change", "delete", "view")
-    proxy: ClassVar[bool]  # default: False
-    required_db_features: ClassVar[List[str]]
-    required_db_vendor: ClassVar[Literal["sqlite", "postgresql", "mysql", "oracle"]]
-    select_on_save: ClassVar[bool]  # default: False
-    indexes: ClassVar[List[Index]]
-    unique_together: ClassVar[Union[Sequence[Sequence[str]], Sequence[str]]]
-    index_together: ClassVar[Union[Sequence[Sequence[str]], Sequence[str]]]  # Deprecated in Django 4.2
-    constraints: ClassVar[List[BaseConstraint]]
-    verbose_name: ClassVar[StrOrPromise]
-    verbose_name_plural: ClassVar[StrOrPromise]
+        abstract: ClassVar[bool]  # default: False
+        app_label: ClassVar[str]
+        base_manager_name: ClassVar[str]
+        db_table: ClassVar[str]
+        db_table_comment: ClassVar[str]
+        db_tablespace: ClassVar[str]
+        default_manager_name: ClassVar[str]
+        default_related_name: ClassVar[str]
+        get_latest_by: ClassVar[Union[str, Sequence[str]]]
+        managed: ClassVar[bool]  # default: True
+        order_with_respect_to: ClassVar[str]
+        ordering: ClassVar[Sequence[str]]
+        permissions: ClassVar[List[Tuple[str, str]]]
+        default_permissions: ClassVar[Sequence[str]]  # default: ("add", "change", "delete", "view")
+        proxy: ClassVar[bool]  # default: False
+        required_db_features: ClassVar[List[str]]
+        required_db_vendor: ClassVar[Literal["sqlite", "postgresql", "mysql", "oracle"]]
+        select_on_save: ClassVar[bool]  # default: False
+        indexes: ClassVar[List[Index]]
+        unique_together: ClassVar[Union[Sequence[Sequence[str]], Sequence[str]]]
+        index_together: ClassVar[Union[Sequence[Sequence[str]], Sequence[str]]]  # Deprecated in Django 4.2
+        constraints: ClassVar[List[BaseConstraint]]
+        verbose_name: ClassVar[StrOrPromise]
+        verbose_name_plural: ClassVar[StrOrPromise]
+
+else:
+    BaseModelMeta = object
