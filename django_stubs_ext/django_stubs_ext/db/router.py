489427f2392ee21ac847d87cca75d75d61d29718
Marti Raudsepp <marti@juffo.org>
2023-06-05 19:03:23 +0300

    Old style optional & type

--- django_stubs_ext/django_stubs_ext/db/router.py
+++ django_stubs_ext/django_stubs_ext/db/router.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, Type
 
 if TYPE_CHECKING:
     from typing import Any
@@ -14,16 +14,18 @@ if TYPE_CHECKING:
         Django documentation: https://docs.djangoproject.com/en/dev/topics/db/multi-db/#automatic-database-routing
         """
 
-        def db_for_read(self, model: type[Model], **hints: Any) -> str | None:
+        def db_for_read(self, model: Type[Model], **hints: Any) -> Optional[str]:
             ...
 
-        def db_for_write(self, model: type[Model], **hints: Any) -> str | None:
+        def db_for_write(self, model: Type[Model], **hints: Any) -> Optional[str]:
             ...
 
-        def allow_relation(self, obj1: type[Model], obj2: type[Model], **hints: Any) -> bool | None:
+        def allow_relation(self, obj1: Type[Model], obj2: Type[Model], **hints: Any) -> Optional[bool]:
             ...
 
-        def allow_migrate(self, db: str, app_label: str, model_name: str | None = None, **hints: Any) -> bool | None:
+        def allow_migrate(
+            self, db: str, app_label: str, model_name: Optional[str] = None, **hints: Any
+        ) -> Optional[bool]:
             ...
 
 else:
