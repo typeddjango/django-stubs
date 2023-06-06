6f5fbefcbbcfb331a6b7a458f2a35febd9c190a2
Marti Raudsepp <marti@juffo.org>
2023-06-06 09:56:44 +0300

    django-stubs-ext: Add TypedDatabaseRouter as database router base class (#1522)
    Base class for Django database routers. Django documentation:
    https://docs.djangoproject.com/en/stable/topics/db/multi-db/#automatic-database-routing
    
    Modeled after the TypedModelMeta feature
    
    Co-authored-by: Adam Johnson <me@adamj.eu>

---
+++ django_stubs_ext/django_stubs_ext/db/router.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING, Optional, Type
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from django.db.models import Model
+
+    class TypedDatabaseRouter:
+        """
+        Typed base class for Django's DATABASE_ROUTERS setting. At runtime this is just an alias to `object`.
+
+        All methods are optional.
+
+        Django documentation: https://docs.djangoproject.com/en/stable/topics/db/multi-db/#automatic-database-routing
+        """
+
+        def db_for_read(self, model: Type[Model], **hints: Any) -> Optional[str]:
+            ...
+
+        def db_for_write(self, model: Type[Model], **hints: Any) -> Optional[str]:
+            ...
+
+        def allow_relation(self, obj1: Type[Model], obj2: Type[Model], **hints: Any) -> Optional[bool]:
+            ...
+
+        def allow_migrate(
+            self, db: str, app_label: str, model_name: Optional[str] = None, **hints: Any
+        ) -> Optional[bool]:
+            ...
+
+else:
+    TypedDatabaseRouter = object
