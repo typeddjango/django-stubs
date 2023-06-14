f2a13a11982e0046edcc715bcfd211a9b3317329
filbasi <filip@netsome.io>
2023-06-14 15:10:10 +0200

    Fix types in DatabaseWraper

--- django-stubs/contrib/gis/db/backends/spatialite/base.pyi
+++ django-stubs/contrib/gis/db/backends/spatialite/base.pyi
@@ -1,15 +1,20 @@
 from typing import Any
 
-from django.contrib.gis.db.backends.spatialite.operations import SpatiaLiteOperations
 from django.db.backends.sqlite3.base import DatabaseWrapper as SQLiteDatabaseWrapper
 
+from .client import SpatiaLiteClient
+from .features import DatabaseFeatures
+from .introspection import SpatiaLiteIntrospection
+from .operations import SpatiaLiteOperations
+from .schema import SpatialiteSchemaEditor
+
 class DatabaseWrapper(SQLiteDatabaseWrapper):
-    SchemaEditorClass: Any
-    client_class: Any
-    features_class: Any
-    introspection_class: Any
-    ops_class: Any
-    lib_spatialite_paths: Any
+    SchemaEditorClass: SpatialiteSchemaEditor
+    client_class: type[SpatiaLiteClient]
+    features_class: type[DatabaseFeatures]
+    introspection_class: type[SpatiaLiteIntrospection]
+    ops_class: type[SpatiaLiteOperations]
+    lib_spatialite_paths: str
     ops: SpatiaLiteOperations
     def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     def get_new_connection(self, conn_params: Any) -> Any: ...
