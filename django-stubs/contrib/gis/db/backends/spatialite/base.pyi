ce3e3becb95a36b8d115fa650f0037e70d7c7156
filbasi <filip@netsome.io>
2023-06-14 12:09:16 +0200

    Add SpatiaLiteOperations ops type in DatabaseWrapper

--- django-stubs/contrib/gis/db/backends/spatialite/base.pyi
+++ django-stubs/contrib/gis/db/backends/spatialite/base.pyi
@@ -1,5 +1,6 @@
 from typing import Any
 
+from django.contrib.gis.db.backends.spatialite.operations import SpatiaLiteOperations
 from django.db.backends.sqlite3.base import DatabaseWrapper as SQLiteDatabaseWrapper
 
 class DatabaseWrapper(SQLiteDatabaseWrapper):
@@ -9,6 +10,7 @@ class DatabaseWrapper(SQLiteDatabaseWrapper):
     introspection_class: Any
     ops_class: Any
     lib_spatialite_paths: Any
+    ops: SpatiaLiteOperations
     def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     def get_new_connection(self, conn_params: Any) -> Any: ...
     def prepare_database(self) -> None: ...
