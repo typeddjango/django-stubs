34b58a9011706a1cbed9ded6b1acd9a17f7d13f2
sobolevn <mail@sobolevn.me>
2023-06-21 15:07:39 +0300

    Add -ignore-positional-only for now

--- scripts/stubtest.sh
+++ scripts/stubtest.sh
@@ -8,5 +8,6 @@ export MYPYPATH='.'
 
 stubtest django \
     --mypy-config-file mypy.ini \
+    --ignore-positional-only \
     --allowlist scripts/stubtest/allowlist.txt \
     --allowlist scripts/stubtest/allowlist_generated.txt
