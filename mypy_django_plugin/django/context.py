73b630997ae569ad7e5415c1f00ad724d8581536
sobolevn <mail@sobolevn.me>
2023-06-22 14:03:51 +0300

    Add compat code

--- mypy_django_plugin/django/context.py
+++ mypy_django_plugin/django/context.py
@@ -73,6 +73,9 @@ class LookupsAreUnsupported(Exception):
 
 class DjangoContext:
     def __init__(self, plugin_config: DjangoPluginConfig) -> None:
+        if isinstance(plugin_config, str):
+            raise RuntimeError('Old API is used: we now require DjangoPluginConfig instance')
+
         self.plugin_config = plugin_config
 
         apps, settings = initialize_django(self.plugin_config.django_settings_module)
