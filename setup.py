028826305cc70bc7943c3bdafdd265f6425f6270
sobolevn <mail@sobolevn.me>
2023-09-05 11:15:09 +0300

    Version 4.2.4 release

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras_require = {
 
 setup(
     name="django-stubs",
-    version="4.2.3",
+    version="4.2.4",
     description="Mypy stubs for Django",
     long_description=readme,
     long_description_content_type="text/markdown",
