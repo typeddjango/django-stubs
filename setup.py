73098ae0acc3fa5cd0cbe6f2880668bd16bd37c3
Marti Raudsepp <marti@juffo.org>
2023-10-11 10:40:57 +0300

    mypy==1.6.*

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ dependencies = [
 
 # Keep compatible-mypy major.minor version pinned to what we use in CI (requirements.txt)
 extras_require = {
-    "compatible-mypy": ["mypy>=1.5,<1.7"],
+    "compatible-mypy": ["mypy==1.6.*"],
 }
 
 setup(
