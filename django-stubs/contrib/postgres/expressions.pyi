018ef702ad7c5652d59a1ea80536df23fc636e86
Petter Friberg <petter_friberg@hotmail.com>
2023-10-11 20:10:53 +0200

    fixup! Add stubs for `django.contrib.postgres.expressions`

--- django-stubs/contrib/postgres/expressions.pyi
+++ django-stubs/contrib/postgres/expressions.pyi
@@ -1,8 +1,9 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db.models import Subquery
+from django.utils.functional import cached_property
 
 class ArraySubquery(Subquery):
     template: str
 
-    @property
+    @cached_property
     def output_field(self) -> ArrayField: ...
