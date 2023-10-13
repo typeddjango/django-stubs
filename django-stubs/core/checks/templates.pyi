b79d155c948c3c4e83b33715530e62da80d9e12e
Thibaut Decombe <thibaut.decombe@gmail.com>
2023-10-13 22:03:23 +0200

    Fix template warnings

--- django-stubs/core/checks/templates.pyi
+++ django-stubs/core/checks/templates.pyi
@@ -6,7 +6,7 @@ from django.core.checks.messages import Error, Warning
 
 E001: Error
 E002: Error
-W001: Warning
+W003: Warning
 
 def check_setting_app_dirs_loaders(app_configs: Sequence[AppConfig] | None, **kwargs: Any) -> Sequence[Error]: ...
 def check_string_if_invalid_is_string(app_configs: Sequence[AppConfig] | None, **kwargs: Any) -> Sequence[Error]: ...
