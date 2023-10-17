997677a6e117da60d25ffc3edce6be8c6cf5f46a
Thibaut Decombe <thibaut.decombe@gmail.com>
2023-10-17 20:55:47 +0200

    Fix after CR

--- django-stubs/core/cache/backends/filebased.pyi
+++ django-stubs/core/cache/backends/filebased.pyi
@@ -3,6 +3,6 @@ from typing import Any, ClassVar
 from django.core.cache.backends.base import BaseCache
 
 class FileBasedCache(BaseCache):
-    cache_suffix: str
+    cache_suffix: ClassVar[str]
     pickle_protocol: ClassVar[int]
     def __init__(self, dir: str, params: dict[str, Any]) -> None: ...
