0f7d40d239e54a9695273d5a3981cb11c87ad7c9
Thibaut Decombe <thibaut.decombe@gmail.com>
2023-11-06 23:18:53 +0100

    Fix some mypy override

--- django-stubs/db/models/expressions.pyi
+++ django-stubs/db/models/expressions.pyi
@@ -194,7 +194,7 @@ class Star(Expression): ...
 class Col(Expression):
     target: Field
     alias: str
-    contains_column_references: Literal[True]
+    contains_column_references: Literal[True]  # type: ignore[assignment]
     possibly_multivalued: Literal[False]
     def __init__(self, alias: str, target: Field, output_field: Field | None = ...) -> None: ...
 
