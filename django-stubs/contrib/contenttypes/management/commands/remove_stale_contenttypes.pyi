374d67f42a07df21bc72d6a44f72ae8e9173b765
Marti Raudsepp <marti@juffo.org>
2023-11-07 13:01:51 +0200

    Improve Collector and NestedObjects attributes, methods

--- django-stubs/contrib/contenttypes/management/commands/remove_stale_contenttypes.pyi
+++ django-stubs/contrib/contenttypes/management/commands/remove_stale_contenttypes.pyi
@@ -1,15 +1,12 @@
-from typing import Any
+from _typeshed import Unused
+from typing import Literal
+
+from django.db.models.deletion import Collector
 
 from django.core.management import BaseCommand
-from django.db.models.base import Model
-from django.db.models.deletion import Collector
+
 
 class Command(BaseCommand): ...
 
 class NoFastDeleteCollector(Collector):
-    data: dict[type[Model], set[Model] | list[Model]]
-    dependencies: dict[Any, Any]
-    fast_deletes: list[Any]
-    field_updates: dict[Any, Any]
-    using: str
-    def can_fast_delete(self, *args: Any, **kwargs: Any) -> bool: ...
+    def can_fast_delete(self, *args: Unused, **kwargs: Unused) -> Literal[False]: ...
