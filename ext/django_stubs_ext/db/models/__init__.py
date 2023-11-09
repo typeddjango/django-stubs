bbb90adb4a527fd279aa8aeb713f1bc3677c99ab
Marti Raudsepp <marti@juffo.org>
2023-11-09 13:16:21 +0200

    Move to django_stubs_ext.db.models.managers

--- ext/django_stubs_ext/db/models/__init__.py
+++ ext/django_stubs_ext/db/models/__init__.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING
 
-# Define TypedModelMeta
 if TYPE_CHECKING:
     from typing import ClassVar, List, Literal, Sequence, Tuple, Union
 
@@ -43,26 +42,3 @@ if TYPE_CHECKING:
 
 else:
     TypedModelMeta = object
-
-
-# Re-export stubs-only classes RelatedManger and ManyRelatedManager.
-# These are fake, Django defines these inside function body.
-if TYPE_CHECKING:
-    # noinspection PyUnresolvedReferences
-    from django.db.models.fields.related_descriptors import ManyRelatedManager as ManyRelatedManager
-
-    # noinspection PyUnresolvedReferences
-    from django.db.models.fields.related_descriptors import RelatedManager as RelatedManager
-
-else:
-    from typing import Protocol, TypeVar
-
-    _T = TypeVar("_T")
-
-    # Define as `Protocol` to prevent them being used with `isinstance()`.
-    # These actually inherit from `BaseManager`.
-    class RelatedManager(Protocol[_T]):
-        pass
-
-    class ManyRelatedManager(Protocol[_T]):
-        pass
