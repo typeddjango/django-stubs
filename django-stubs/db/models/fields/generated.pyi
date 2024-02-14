a27ff8e50107ea17ebd60763885984d2a3f54c32
Tom Parker-Shemilt <palfrey@tevp.net>
2024-02-14 23:06:08 +0000

    Fix all the known test failures for GeneratedField

--- django-stubs/db/models/fields/generated.pyi
+++ django-stubs/db/models/fields/generated.pyi
@@ -1,13 +1,47 @@
-from typing import Any
+from typing import Any, Iterable, Literal
 
+from django.core.validators import _ValidatorCallable
 from django.db import models
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models.expressions import Expression
+from django.db.models.fields import _ErrorMessagesMapping, _FieldChoices
+from django.db.models.sql import Query
 from django.utils.datastructures import DictWrapper
+from django.utils.functional import _StrOrPromise
 
 class GeneratedField(models.Field):
+    generated: Literal[True] = True
+    db_returning: Literal[True] = True
+    _query: Query | None = None
+    output_field: models.Field | None = None
+
     def __init__(
-        self, *, expression: Expression, output_field: models.Field, db_persist: bool | None = ..., **kwargs: Any
+        self,
+        *,
+        expression: Expression,
+        output_field: models.Field,
+        db_persist: bool | None = ...,
+        verbose_name: _StrOrPromise | None = ...,
+        name: str | None = ...,
+        primary_key: bool = ...,
+        unique: bool = ...,
+        blank: bool = ...,
+        null: bool = ...,
+        default: Any = ...,
+        editable: bool = ...,
+        auto_created: bool = ...,
+        serialize: bool = ...,
+        unique_for_date: str | None = ...,
+        unique_for_month: str | None = ...,
+        unique_for_year: str | None = ...,
+        choices: _FieldChoices | None = ...,
+        help_text: _StrOrPromise = ...,
+        db_column: str | None = ...,
+        db_comment: str | None = ...,
+        db_tablespace: str | None = ...,
+        validators: Iterable[_ValidatorCallable] = ...,
+        error_messages: _ErrorMessagesMapping | None = ...,
+        **kwargs: Any,
     ): ...
     def generated_sql(self, connection: BaseDatabaseWrapper) -> tuple[str, Any]: ...
     def db_type_parameters(self, connection: BaseDatabaseWrapper) -> DictWrapper: ...
