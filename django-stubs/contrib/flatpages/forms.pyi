bcc175be1c87406e4a4fc105e1869208ac909b04
jorenham <jhammudoglu@gmail.com>
2024-03-11 22:08:15 +0100

    add missing `Meta` for specific forms

--- django-stubs/contrib/flatpages/forms.pyi
+++ django-stubs/contrib/flatpages/forms.pyi
@@ -1,7 +1,14 @@
-from typing import Any
+from typing import Any, Literal, Sequence, TypeVar
 
 from django import forms
+from django.contrib.flatpages.models import FlatPage
 
-class FlatpageForm(forms.ModelForm):
+_FlatPage = TypeVar("_FlatPage", bound=FlatPage)
+
+class FlatpageForm(forms.ModelForm[_FlatPage]):
+    class Meta:
+        model: type[_FlatPage] = ...
+        fields: Sequence[str] | Literal['__all__'] = ...
+
     url: Any
     def clean_url(self) -> str: ...
