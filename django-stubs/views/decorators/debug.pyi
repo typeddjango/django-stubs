2dad6bba0dc5dcb3bdcd85d5422304ea9cb1208b
Nick Pope <nick@nickpope.me.uk>
2024-03-13 20:22:26 +0000

    fixup! 5.0: Update `django.views`

--- django-stubs/views/decorators/debug.pyi
+++ django-stubs/views/decorators/debug.pyi
@@ -1,9 +1,9 @@
 from collections.abc import Callable
-from typing import Any, TypeVar
+from typing import Any, Literal, TypeVar
 
 _F = TypeVar("_F", bound=Callable[..., Any])
 
-coroutine_functions_to_sensitive_variables: dict[int, str | tuple[str, ...]]
+coroutine_functions_to_sensitive_variables: dict[int, Literal["__ALL__"] | tuple[str, ...]]
 
 def sensitive_variables(*variables: str) -> Callable[[_F], _F]: ...
 def sensitive_post_parameters(*parameters: str) -> Callable[[_F], _F]: ...
