5785352f6f3996b8119ee26f538b10224753e406
Nick Pope <nick@nickpope.me.uk>
2024-03-13 19:32:53 +0000

    5.0: Update `django.views`

--- django-stubs/views/decorators/debug.pyi
+++ django-stubs/views/decorators/debug.pyi
@@ -3,5 +3,7 @@ from typing import Any, TypeVar
 
 _F = TypeVar("_F", bound=Callable[..., Any])
 
+coroutine_functions_to_sensitive_variables: dict[int, str | tuple[str, ...]]
+
 def sensitive_variables(*variables: str) -> Callable[[_F], _F]: ...
 def sensitive_post_parameters(*parameters: str) -> Callable[[_F], _F]: ...
