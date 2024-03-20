a13f68e707a50f9ed5ceddeb13cdcd3c9562fd32
Alexandre Spaeth <Alexerson@users.noreply.github.com>
2024-03-20 12:52:23 -0700

    Adding missing `Q` methods: `check()`, `flatten()` (#1899)
    Co-authored-by: Victorien <65306057+Viicos@users.noreply.github.com>

--- django-stubs/db/models/query_utils.pyi
+++ django-stubs/db/models/query_utils.pyi
@@ -2,6 +2,7 @@ from collections import namedtuple
 from collections.abc import Collection, Iterable, Iterator, Mapping, Sequence
 from typing import Any, ClassVar, Literal, TypeVar
 
+from _typeshed import Incomplete
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models.base import Model
 from django.db.models.expressions import BaseExpression
@@ -40,6 +41,8 @@ class Q(tree.Node):
         summarize: bool = ...,
         for_save: bool = ...,
     ) -> WhereNode: ...
+    def flatten(self) -> Iterator[Incomplete]: ...
+    def check(self, against: dict[str, Any], using: str = ...) -> bool: ...
     def deconstruct(self) -> tuple[str, Sequence[Any], dict[str, Any]]: ...
 
 class DeferredAttribute:
