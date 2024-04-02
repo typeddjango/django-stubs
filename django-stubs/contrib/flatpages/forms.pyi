304ea1f97f439b2681df938c0469531e4f594dbe
jorenham <jhammudoglu@gmail.com>
2024-04-02 16:09:35 +0200

    undo misplaced typing fix

--- django-stubs/contrib/flatpages/forms.pyi
+++ django-stubs/contrib/flatpages/forms.pyi
@@ -1,8 +1,7 @@
 from typing import Any
 
 from django import forms
-from django.contrib.flatpages.models import FlatPage
 
-class FlatpageForm(forms.ModelForm[FlatPage]):
+class FlatpageForm(forms.ModelForm):
     url: Any
     def clean_url(self) -> str: ...
