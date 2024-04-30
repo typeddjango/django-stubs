64a3aac4c0cc59da6088ca166550f059d0de8bf9
Marti Raudsepp <marti@juffo.org>
2024-04-30 11:53:42 +0300

    Remove incorrect `Reversible` base class from `QuerySet` (#2094)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- django-stubs/db/models/query.pyi
+++ django-stubs/db/models/query.pyi
@@ -1,5 +1,5 @@
 import datetime
-from collections.abc import AsyncIterator, Collection, Iterable, Iterator, MutableMapping, Reversible, Sequence, Sized
+from collections.abc import AsyncIterator, Collection, Iterable, Iterator, MutableMapping, Sequence, Sized
 from typing import Any, Generic, NamedTuple, TypeVar, overload
 
 from django.db.backends.utils import _ExecuteQuery
@@ -43,7 +43,7 @@ class NamedValuesListIterable(ValuesListIterable[NamedTuple]):
 class FlatValuesListIterable(BaseIterable[_Row]):
     def __iter__(self) -> Iterator[_Row]: ...
 
-class _QuerySet(Generic[_T, _Row], Reversible[_Row], Iterable[_Row], Sized):
+class _QuerySet(Generic[_T, _Row], Iterable[_Row], Sized):
     model: type[_T]
     query: Query
     _iterable_class: type[BaseIterable]
