d19d12420cb1113cfb23b3239fe9fccf96ee3aaf
Marti Raudsepp <marti@juffo.org>
2024-05-01 00:00:53 +0300

    Remove QuerySet alias hacks via PEP 696 TypeVar defaults
    The `QuerySet` class was previously named `_QuerySet` and had three aliases: `QuerySet`, `QuerySetAny` and `ValuesQuerySet`.
    
    These hacks were mainly needed to for the ergonomic single-parameter `QuerySet[Model]`, which expanded into `_QuerySet[Model, Model]`
    
    But now that mypy 1.10 implements PEP 696 to a fuller extent (Pyright also supports it), the 2nd type parameter can be a simple TypeVar that defaults to 1st type parameter.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ dependencies = [
     "django-stubs-ext>=5.0.0",
     "tomli; python_version < '3.11'",
     # Types:
-    "typing-extensions",
+    "typing-extensions>=4.11.0",
     "types-PyYAML",
 ]
 
