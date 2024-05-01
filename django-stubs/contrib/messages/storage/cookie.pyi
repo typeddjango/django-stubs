38285f1c2c297a06dc18b9490da91decde1c9741
Jae Hyuck Sa <wogur981208@gmail.com>
2024-05-02 00:16:19 +0900

    5.0: Update django.contrib.messages.storage (#2106)

--- django-stubs/contrib/messages/storage/cookie.pyi
+++ django-stubs/contrib/messages/storage/cookie.pyi
@@ -1,5 +1,5 @@
 import json
-from typing import Any
+from typing import Any, Callable, Sequence
 
 from django.contrib.messages.storage.base import BaseStorage
 
@@ -14,7 +14,20 @@ class MessageEncoder(json.JSONEncoder):
 class MessageDecoder(json.JSONDecoder):
     def process_messages(self, obj: Any) -> Any: ...
 
+class MessagePartSerializer:
+    def dumps(self, obj: Any) -> Sequence[str]: ...
+
+class MessagePartGatherSerializer:
+    def dumps(self, obj: Any) -> bytes: ...
+
+class MessageSerializer:
+    def loads(self, data: bytes | bytearray) -> Any: ...
+
 class CookieStorage(BaseStorage):
     cookie_name: str
     max_cookie_size: int
     not_finished: str
+    not_finished_json: str
+
+def bisect_keep_left(a: list[int], fn: Callable[[list[int]], bool]) -> int: ...
+def bisect_keep_right(a: list[int], fn: Callable[[list[int]], bool]) -> int: ...
