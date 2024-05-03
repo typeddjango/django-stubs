a86ad96789c6d0fffa875aea07824f24e53e8d71
Jae Hyuck Sa <wogur981208@gmail.com>
2024-05-04 04:09:23 +0900

    Update django_tests_settings.py

--- scripts/django_tests_settings.py
+++ scripts/django_tests_settings.py
@@ -11,7 +11,3 @@ INSTALLED_APPS = [
 ]
 
 STATIC_URL = "static/"
-
-
-GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib"
-GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib"
