5118c5dfbe99504e1cca57ea00bf38bd14014e4f
Petter Friberg <petter_friberg@hotmail.com>
2024-05-27 14:12:05 +0200

    Bump `django-stubs-ext` dependency to latest (#2186)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md") as f:
 dependencies = [
     "django",
     "asgiref",
-    "django-stubs-ext>=5.0.0",
+    "django-stubs-ext>=5.0.2",
     "tomli; python_version < '3.11'",
     # Types:
     "typing-extensions>=4.11.0",
@@ -39,7 +39,7 @@ extras_require = {
 
 setup(
     name="django-stubs",
-    version="5.0.1",
+    version="5.0.2",
     description="Mypy stubs for Django",
     long_description=readme,
     long_description_content_type="text/markdown",
