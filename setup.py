d9b47afb1b6ac1ce025282289d7382ba5e4845f2
Petter Friberg <petter_friberg@hotmail.com>
2024-05-27 13:12:57 +0200

    Prepare for a release `5.0.1` (#2183)

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extras_require = {
 
 setup(
     name="django-stubs",
-    version="5.0.0",
+    version="5.0.1",
     description="Mypy stubs for Django",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -69,7 +69,6 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Typing :: Typed",
         "Framework :: Django",
-        "Framework :: Django :: 4.1",
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
     ],
