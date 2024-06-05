01618e4509abc7ad6285fc6d2fcd23a100b55f70
Petter Friberg <petter_friberg@hotmail.com>
2024-06-05 12:53:10 +0200

    Migrate `db/models/test_constraints.yml` to 'assert_type' tests (#2204)

---
+++ tests/assert_type/db/models/test_constraints.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from django.db.models import CheckConstraint, F, UniqueConstraint
+from django.db.models.functions import Lower
+from django.db.models.lookups import LessThan
+
+UniqueConstraint(Lower("name").desc(), "category", name="unique_lower_name_category")
+UniqueConstraint(fields=["name"], name="unqiue_name")
+# There's no overload case for passing both expression and 'fields'
+UniqueConstraint(  # type: ignore[call-overload]
+    Lower("name"),
+    fields=["name"],  # pyright: ignore[reportArgumentType]
+    name="unique_mess",
+)
+
+CheckConstraint(name="less_than_constraint", check=LessThan[Any](F("months"), 1))
