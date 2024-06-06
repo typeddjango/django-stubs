0f835788e03f0c09db6faebda9218c5ce5cd447c
Petter Friberg <petter_friberg@hotmail.com>
2024-06-06 11:06:47 +0200

    fixup! Tests: Migrate `db/test_transaction.yml` to 'assert_type' tests

--- tests/assert_type/db/test_transaction.py
+++ tests/assert_type/db/test_transaction.py
@@ -1,33 +1,33 @@
 from collections.abc import Callable
+from typing import List
 
 from django.db.transaction import atomic, non_atomic_requests
 from django.http import HttpRequest, HttpResponse
-from typing_extensions import assert_type
 
 
 @atomic
-def func1(x: int, /) -> list[int]: ...
+def func1(x: int) -> List[int]: ...
 
 
-assert_type(func1, Callable[[int], list[int]])
+x1: Callable[[int], List[int]] = func1
 
 
 @atomic(using="bla", savepoint=False)
-def func2(x: int, /) -> list[int]: ...
+def func2(x: int) -> List[int]: ...
 
 
-assert_type(func2, Callable[[int], list[int]])
-
-
-@non_atomic_requests
-def view_func1(request: HttpRequest, /) -> HttpResponse: ...
-
-
-assert_type(view_func1, Callable[[HttpRequest], HttpResponse])
+x1 = func2
 
 
 @non_atomic_requests
-def view_func2(request: HttpRequest, arg: str, /) -> HttpResponse: ...
+def view_func1(request: HttpRequest) -> HttpResponse: ...
 
 
-assert_type(view_func2, Callable[[HttpRequest, str], HttpResponse])
+x2: Callable[[HttpRequest], HttpResponse] = view_func1
+
+
+@non_atomic_requests
+def view_func2(request: HttpRequest, arg: str) -> HttpResponse: ...
+
+
+x3: Callable[[HttpRequest, str], HttpResponse] = view_func2
