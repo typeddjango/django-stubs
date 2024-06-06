2d05cfc315c1809f1d92de2f2481f39d9d92e256
Petter Friberg <petter_friberg@hotmail.com>
2024-06-06 22:44:25 +0200

    Allow any function for admin's `display` and `action` decorators (#2210)
    I realised that:
    - The `display` decorator isn't limited to any specific type of
      callable, it accepts anything really.
    - The `action` decorator was trying to dictate a callable type that the
      `ModelAdmin.actions` expects. But that's the responsibility of
      `ModelAdmin`.

--- django-stubs/contrib/admin/decorators.pyi
+++ django-stubs/contrib/admin/decorators.pyi
@@ -3,45 +3,33 @@ from typing import Any, TypeVar, overload
 
 from django.contrib.admin import ModelAdmin
 from django.contrib.admin.sites import AdminSite
-from django.db.models import QuerySet
 from django.db.models.base import Model
 from django.db.models.expressions import BaseExpression, Combinable
-from django.http import HttpRequest, HttpResponseBase
 from django.utils.functional import _StrOrPromise
-from typing_extensions import TypeAlias
 
-_Model = TypeVar("_Model", bound=Model)
-_ModelAdmin = TypeVar("_ModelAdmin", bound=ModelAdmin)
-_Request = TypeVar("_Request", bound=HttpRequest)
-_QuerySet = TypeVar("_QuerySet", bound=QuerySet)
-# This is deliberately different from _DisplayT defined in contrib.admin.options
-_DisplayCallable: TypeAlias = Callable[[_ModelAdmin, _Model], Any] | Callable[[_Model], Any]
-_DisplayCallableT = TypeVar("_DisplayCallableT", bound=_DisplayCallable)
-_ActionReturn = TypeVar("_ActionReturn", bound=HttpResponseBase | None)
+_ModelAdmin = TypeVar("_ModelAdmin", bound=ModelAdmin[Any])
+_F = TypeVar("_F", bound=Callable[..., Any])
 
 @overload
 def action(
-    function: Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn],
+    function: _F,
     permissions: Sequence[str] | None = ...,
     description: _StrOrPromise | None = ...,
-) -> Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn]: ...
+) -> _F: ...
 @overload
 def action(
     *,
     permissions: Sequence[str] | None = ...,
     description: _StrOrPromise | None = ...,
-) -> Callable[
-    [Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn]],
-    Callable[[_ModelAdmin, _Request, _QuerySet], _ActionReturn],
-]: ...
+) -> Callable[[_F], _F]: ...
 @overload
 def display(
-    function: _DisplayCallableT,
+    function: _F,
     boolean: bool | None = ...,
     ordering: str | Combinable | BaseExpression | None = ...,
     description: _StrOrPromise | None = ...,
     empty_value: str | None = ...,
-) -> _DisplayCallableT: ...
+) -> _F: ...
 @overload
 def display(
     *,
@@ -49,7 +37,7 @@ def display(
     ordering: str | Combinable | BaseExpression | None = ...,
     description: _StrOrPromise | None = ...,
     empty_value: str | None = ...,
-) -> Callable[[_DisplayCallableT], _DisplayCallableT]: ...
+) -> Callable[[_F], _F]: ...
 def register(
     *models: type[Model], site: AdminSite | None = ...
 ) -> Callable[[type[_ModelAdmin]], type[_ModelAdmin]]: ...
