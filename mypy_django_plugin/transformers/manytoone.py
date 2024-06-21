394bec1e81a661cc580a61e4df1aa48cef5eee00
Petter Friberg <petter_friberg@hotmail.com>
2024-06-21 15:15:46 +0200

    fixup! fixup! Make `ReverseManyToOneDescriptor` generic over a model

--- mypy_django_plugin/transformers/manytoone.py
+++ mypy_django_plugin/transformers/manytoone.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional
 
 from mypy.plugin import MethodContext
 from mypy.types import Instance
@@ -7,16 +7,13 @@ from mypy.types import Type as MypyType
 from mypy_django_plugin.lib import fullnames, helpers
 
 
-def get_related_manager_and_model(ctx: MethodContext) -> Optional[Tuple[Instance, Instance]]:
+def get_model_of_related_manager(ctx: MethodContext) -> Optional[Instance]:
     """
-    Returns a 2-tuple consisting of:
-      1. A `RelatedManager` instance
-      2. The type parameter (_To) instance of 1. when it's a model
-    When encountering a `RelatedManager` that has populated its first type parameter
-    with a model. Otherwise `None` is returned.
+    Returns the type parameter (_To) instance of a `RelatedManager` instance when it's a
+    model. Otherwise `None` is returned.
 
-    For example: if given a `RelatedManager[A]` where `A` is a model the following
-    2-tuple is returned: `(RelatedManager[A], A)`.
+    For example: if given a `RelatedManager[A]` where `A` is a model then `A` is
+    returned.
     """
     if (
         isinstance(ctx.default_return_type, Instance)
@@ -32,7 +29,7 @@ def get_related_manager_and_model(ctx: MethodContext) -> Optional[Tuple[Instance
             and isinstance(related_manager.args[0], Instance)
             and helpers.is_model_type(related_manager.args[0].type)
         ):
-            return related_manager, related_manager.args[0]
+            return related_manager.args[0]
 
     return None
 
@@ -42,11 +39,10 @@ def refine_many_to_one_related_manager(ctx: MethodContext) -> MypyType:
     Updates the 'RelatedManager' returned by e.g. 'ReverseManyToOneDescriptor' to be a subclass
     of 'RelatedManager' and the related model's default manager.
     """
-    related_objects = get_related_manager_and_model(ctx)
-    if related_objects is None:
+    to_model_instance = get_model_of_related_manager(ctx)
+    if to_model_instance is None:
         return ctx.default_return_type
 
-    related_manager, to_model_instance = related_objects
     checker = helpers.get_typechecker_api(ctx)
     related_manager_info = helpers.get_reverse_manager_info(
         checker, to_model_instance.type, derived_from="_default_manager"
