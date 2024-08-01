972a4e529b733820c2d9317310b279c6823b3f41
sobolevn <mail@sobolevn.me>
2024-08-01 11:23:25 +0300

    Update pyright to 1.1.374 (#2305)

--- tests/assert_type/db/models/fields/test_related_descriptors.py
+++ tests/assert_type/db/models/fields/test_related_descriptors.py
@@ -13,6 +13,4 @@ class MyModel(models.Model):
     rel = models.ForeignKey[Other, Other](Other, on_delete=models.CASCADE, related_name="explicit_descriptor")
 
 
-# Pyright doesn't allow "runtime" usage of @type_check_only 'RelatedManager' but we're
-# only type checking these files so it should be fine.
-assert_type(Other().explicit_descriptor, RelatedManager[MyModel])  # pyright: ignore[reportGeneralTypeIssues]
+assert_type(Other().explicit_descriptor, RelatedManager[MyModel])
