47a658a2f93adc21f00418e7107cf59f0bad59af
q0w <43147888+q0w@users.noreply.github.com>
2024-08-10 16:59:26 +0300

    Deprecate check

--- django-stubs/db/models/constraints.pyi
+++ django-stubs/db/models/constraints.pyi
@@ -45,6 +45,7 @@ class BaseConstraint:
 class CheckConstraint(BaseConstraint):
     check: Q | BaseExpression
 
+    @deprecated("The check keyword argument is deprecated in favor of condition and will be removed in Django 6.0")
     @overload
     def __init__(
         self,
