509e36813f0ed418f8cbeba9b529292b678ecb49
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2024-10-09 16:40:51 +0200

    Make it explicit that the dummy template class adheres to the protocol

--- django-stubs/template/backends/dummy.pyi
+++ django-stubs/template/backends/dummy.pyi
@@ -3,11 +3,11 @@ from typing import Any
 
 from django.http.request import HttpRequest
 
-from .base import BaseEngine
+from .base import BaseEngine, _EngineTemplate
 
 class TemplateStrings(BaseEngine):
     def __init__(self, params: dict[str, dict[Any, Any] | list[Any] | bool | str]) -> None: ...
 
-class Template(string.Template):
+class Template(string.Template, _EngineTemplate):
     template: str
     def render(self, context: dict[str, Any] | None = ..., request: HttpRequest | None = ...) -> str: ...
