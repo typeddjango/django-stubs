3f9a140dbe4c84986d66438659f1a53c5e2f7f21
Federico Bond <federicobond@gmail.com>
2024-10-15 21:11:27 -0300

    Fix TypedDictType crash with mypy 1.12.x

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ dependencies = [
 
 # Keep compatible-mypy major.minor version pinned to what we use in CI (requirements.txt)
 extras_require = {
-    "compatible-mypy": ["mypy~=1.11.0"],
+    "compatible-mypy": ["mypy~=1.12.0"],
     "redis": ["redis"],
     "oracle": ["oracledb"],
 }
