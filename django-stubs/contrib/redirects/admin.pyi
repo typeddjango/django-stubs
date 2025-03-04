c83ee269424b05e38a1bee07cf8cdc8591aa807c
Yury V. Zaytsev <yury@shurup.com>
2025-03-04 11:42:54 +0100

    Annotate remaining `ModelAdmin` fields with `ClassVar`

--- django-stubs/contrib/redirects/admin.pyi
+++ django-stubs/contrib/redirects/admin.pyi
@@ -3,7 +3,7 @@ from typing import Any, ClassVar
 from django.contrib import admin
 
 class RedirectAdmin(admin.ModelAdmin):
-    list_display: Any
-    list_filter: Any
-    search_fields: Any
+    list_display: ClassVar[Any]
+    list_filter: ClassVar[Any]
+    search_fields: ClassVar[Any]
     radio_fields: ClassVar[Any]
