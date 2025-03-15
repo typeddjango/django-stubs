57755b6b0ff1e6b5c0e8ad7db3ab7a2e3023161b
Adam Johnson <me@adamj.eu>
2025-03-15 22:43:04 +0000

    Complete JSONObject

--- django-stubs/db/models/functions/comparison.pyi
+++ django-stubs/db/models/functions/comparison.pyi
@@ -1,9 +1,11 @@
 from typing import Any, ClassVar
 
+from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models import Func
 from django.db.models.expressions import Combinable
 from django.db.models.fields import Field
 from django.db.models.fields.json import JSONField
+from django.db.models.sql.compiler import SQLCompiler, _AsSqlType
 
 class Cast(Func):
     def __init__(self, expression: Combinable | str, output_field: str | Field) -> None: ...
@@ -16,8 +18,16 @@ class Collate(Func):
 class Greatest(Func): ...
 
 class JSONObject(Func):
-    def __init__(self, **fields: Any) -> None: ...
     output_field: ClassVar[JSONField]
+    def __init__(self, **fields: Any) -> None: ...
+    def as_sql(self, compiler: SQLCompiler, connection: BaseDatabaseWrapper, **extra_context: Any) -> _AsSqlType: ...  # type: ignore [override]
+    def as_native(
+        self, compiler: SQLCompiler, connection: BaseDatabaseWrapper, *, returning: str, **extra_context: Any
+    ) -> _AsSqlType: ...
+    def as_postgresql(
+        self, compiler: SQLCompiler, connection: BaseDatabaseWrapper, **extra_context: Any
+    ) -> _AsSqlType: ...
+    def as_oracle(self, compiler: SQLCompiler, connection: BaseDatabaseWrapper, **extra_context: Any) -> _AsSqlType: ...
 
 class Least(Func): ...
 class NullIf(Func): ...
