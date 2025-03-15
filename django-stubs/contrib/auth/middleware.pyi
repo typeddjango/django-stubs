b1426a64209cc117261eba031346a8d1f081ff2f
Adam Johnson <me@adamj.eu>
2025-03-15 22:36:24 +0000

    Update contrib.auth for Django 5.1 (#2555)
    * Update contrib.auth for Django 5.1
    
    From https://github.com/django/django/commit/00e187961059a0e77403151d2bb38c217101d5af:
    * `django.contrib.auth.base_user.BaseUserManager.make_random_password` removed
    
    From https://github.com/django/django/commit/f64c528c17af91333fc6e96d91b5f13c0fb647d8:
    
    * `django.contrib.auth.forms.AdminPasswordChangeForm.clean_password2` removed
    * `django.contrib.auth.forms.BaseUserCreationForm.clean_password2` removed
    * `django.contrib.auth.forms.SetPasswordForm.clean_new_password2` removed
    * `django.contrib.auth.forms.SetPasswordMixin` added
    
    From https://github.com/django/django/commit/e626716c28b6286f8cf0f8174077f3d2244f3eb3:
    
    * `django.contrib.auth.forms.AdminPasswordChangeForm.usable_password_help_text` added
    
    From https://github.com/django/django/commit/da22e6cb3cdb51f2f7ca45e1591d2c6a8be458e0:
    
    * `django.contrib.auth.forms.SetUnusablePasswordMixin` added
    * `django.contrib.auth.forms.AdminUserCreationForm` added
    
    From https://github.com/django/django/commit/c7fc9f20b49b5889a9a8f47de45165ac443c1a21:
    
    * `django.contrib.auth.middleware.LoginRequiredMiddleware` added
    
    * ClassVar
    
    Co-authored-by: sobolevn <mail@sobolevn.me>
    
    * _ErrorMessagesDict
    
    * import
    
    ---------
    
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- django-stubs/contrib/auth/middleware.pyi
+++ django-stubs/contrib/auth/middleware.pyi
@@ -1,6 +1,8 @@
+from typing import Any, Callable, ClassVar
+
 from django.contrib.auth.base_user import _UserModel
 from django.contrib.auth.models import AnonymousUser
-from django.http.request import HttpRequest
+from django.http import HttpRequest, HttpResponseBase, HttpResponseRedirect
 from django.utils.deprecation import MiddlewareMixin
 
 def get_user(request: HttpRequest) -> AnonymousUser | _UserModel: ...
@@ -8,6 +10,22 @@ async def auser(request: HttpRequest) -> AnonymousUser | _UserModel: ...
 
 class AuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request: HttpRequest) -> None: ...
+
+class LoginRequiredMiddleware(MiddlewareMixin):
+    redirect_field_name: ClassVar[str]
+
+    def process_view(
+        self,
+        request: HttpRequest,
+        view_func: Callable[..., HttpResponseBase],
+        view_args: tuple[Any, ...],
+        view_kwargs: dict[Any, Any],
+    ) -> HttpResponseBase | None: ...
+    def get_login_url(self, view_func: Callable[..., HttpResponseBase]) -> str: ...
+    def get_redirect_field_name(self, view_func: Callable[..., HttpResponseBase]) -> str: ...
+    def handle_no_permission(
+        self, request: HttpRequest, view_func: Callable[..., HttpResponseBase]
+    ) -> HttpResponseRedirect: ...
 
 class RemoteUserMiddleware(MiddlewareMixin):
     header: str
