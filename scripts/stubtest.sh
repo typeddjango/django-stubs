b6eda9378e6413f02cd5533c901eb618fda11348
sobolevn <mail@sobolevn.me>
2025-04-06 12:52:59 +0300

    Update stubtest

--- scripts/stubtest.sh
+++ scripts/stubtest.sh
@@ -13,4 +13,4 @@ stubtest django \
     --mypy-config-file mypy.ini \
     --allowlist scripts/stubtest/allowlist.txt \
     --allowlist scripts/stubtest/allowlist_todo.txt \
-    --allowlist scripts/stubtest/allowlist_django52.txt
+    --allowlist scripts/stubtest/allowlist_todo_django52.txt
