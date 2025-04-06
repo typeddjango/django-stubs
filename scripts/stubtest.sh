de9cc082b940f6d28c6ab396b949de4f6866b2a5
sobolevn <mail@sobolevn.me>
2025-04-06 12:36:35 +0300

    Update django to 5.2

--- scripts/stubtest.sh
+++ scripts/stubtest.sh
@@ -9,10 +9,8 @@ export MYPYPATH='.'
 # Cleaning existing cache:
 rm -rf .mypy_cache
 
-# TODO: remove `--ignore-positional-only` when ready
 stubtest django \
     --mypy-config-file mypy.ini \
-    --ignore-positional-only \
     --allowlist scripts/stubtest/allowlist.txt \
     --allowlist scripts/stubtest/allowlist_todo.txt \
-    --allowlist scripts/stubtest/allowlist_todo_django51.txt
+    --generate-allowlist
