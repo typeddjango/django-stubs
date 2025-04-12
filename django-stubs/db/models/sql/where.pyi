c5316de85590620474121852a1a274cd39df6e53
사재혁 <wogur981208@gmail.com>
2025-04-12 18:20:23 +0900

    Update django.db.models.sql for Django 5.2 (#2606)
    Signed-off-by: SaJH <wogur981208@gmail.com>

--- django-stubs/db/models/sql/where.pyi
+++ django-stubs/db/models/sql/where.pyi
@@ -5,7 +5,6 @@ from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models.expressions import Expression
 from django.db.models.fields import BooleanField
 from django.db.models.sql.compiler import SQLCompiler, _AsSqlType, _ParamsT
-from django.db.models.sql.query import Query
 from django.utils import tree
 from django.utils.functional import cached_property
 
@@ -50,12 +49,3 @@ class ExtraWhere:
     def as_sql(
         self, compiler: SQLCompiler | None = None, connection: BaseDatabaseWrapper | None = None
     ) -> _AsSqlType: ...
-
-class SubqueryConstraint:
-    contains_aggregate: bool
-    alias: str
-    columns: list[str]
-    targets: list[str]
-    query_object: Query
-    def __init__(self, alias: str, columns: list[str], targets: list[str], query_object: Query) -> None: ...
-    def as_sql(self, compiler: SQLCompiler, connection: BaseDatabaseWrapper) -> _AsSqlType: ...
