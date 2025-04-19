bb38e68f4c637db3ba34ef8f05999e7e4bc0e082
SaJH <wogur981208@gmail.com>
2025-04-19 13:36:55 +0900

    Update django.core.serializers for Django 5.2
    Signed-off-by: SaJH <wogur981208@gmail.com>

--- django-stubs/core/serializers/jsonl.pyi
+++ django-stubs/core/serializers/jsonl.pyi
@@ -1,12 +1,10 @@
-from collections.abc import Iterator
 from typing import IO, Any
 
-from django.core.serializers.base import DeserializedObject
+from django.core.serializers.python import Deserializer as PythonDeserializer
 from django.core.serializers.python import Serializer as PythonSerializer
 
 class Serializer(PythonSerializer):
     json_kwargs: dict[str, Any]
 
-def Deserializer(
-    stream_or_string: IO[bytes] | IO[str] | bytes | str, **options: Any
-) -> Iterator[DeserializedObject]: ...
+class Deserializer(PythonDeserializer):
+    def __init__(self, stream_or_string: IO[bytes] | IO[str] | bytes | str, **options: Any) -> None: ...
