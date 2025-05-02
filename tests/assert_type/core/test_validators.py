af7948ecb035a2ab6e5b106c70c5599db80f5107
Thibaut Decombe <thibaut.decombe@gmail.com>
2025-05-03 00:08:00 +0200

    Add pyright ignores too + use `typing_extensions`

--- tests/assert_type/core/test_validators.py
+++ tests/assert_type/core/test_validators.py
@@ -1,18 +1,22 @@
 import re
 from re import Pattern
-from typing import assert_type
 
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.validators import RegexValidator
+from typing_extensions import assert_type
 
 assert_type(RegexValidator.regex, str | Pattern[str])
 assert_type(RegexValidator().regex, Pattern[str])
 RegexValidator().regex = re.compile("")
-RegexValidator().regex = ""  # type: ignore[assignment] # expect "Pattern[str]"
-RegexValidator.regex = "anything fails here"  # type: ignore[assignment] # expect "_ClassOrInstanceAttribute[Union[str, Pattern[str]], Pattern[str]]"
 
 assert_type(UnicodeUsernameValidator.regex, str | Pattern[str])
 assert_type(UnicodeUsernameValidator().regex, Pattern[str])
 UnicodeUsernameValidator().regex = re.compile("")
-UnicodeUsernameValidator().regex = ""  # type: ignore[assignment] # expect "Pattern[str]"
-UnicodeUsernameValidator.regex = "anything fails here"  # type: ignore[assignment] # expect "_ClassOrInstanceAttribute[Union[str, Pattern[str]], Pattern[str]]"
+
+# expect "Pattern[str]"
+RegexValidator().regex = ""  # type: ignore[assignment] # pyright: ignore[reportAttributeAccessIssue]
+UnicodeUsernameValidator().regex = ""  # type: ignore[assignment] # pyright: ignore[reportAttributeAccessIssue]
+
+# expect "_ClassOrInstanceAttribute[Union[str, Pattern[str]], Pattern[str]]"
+RegexValidator.regex = "anything fails here"  # type: ignore[assignment]
+UnicodeUsernameValidator.regex = "anything fails here"  # type: ignore[assignment]
