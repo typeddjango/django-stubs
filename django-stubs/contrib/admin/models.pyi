9c1964023684511f63ddd5f232ae6f512978da33
Alessio Bogon <778703+youtux@users.noreply.github.com>
2025-06-11 14:50:22 +0000

    Add signature overrides for `LogEntryManager.log_actions`

--- django-stubs/contrib/admin/models.pyi
+++ django-stubs/contrib/admin/models.pyi
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-from typing import Any, ClassVar
+from typing import Any, ClassVar, Literal, overload
 from uuid import UUID
 
 from django.db import models
@@ -22,6 +22,7 @@ class LogEntryManager(models.Manager[LogEntry]):
         action_flag: int,
         change_message: Any = ...,
     ) -> LogEntry: ...
+    @overload
     def log_actions(
         self,
         user_id: int | str | UUID,
@@ -29,8 +30,18 @@ class LogEntryManager(models.Manager[LogEntry]):
         action_flag: int,
         change_message: str | list[Any] = "",
         *,
-        single_object: bool = False,
-    ) -> list[LogEntry] | LogEntry: ...
+        single_object: Literal[True] = ...,
+    ) -> LogEntry: ...
+    @overload
+    def log_actions(
+        self,
+        user_id: int | str | UUID,
+        queryset: Iterable[Model],
+        action_flag: int,
+        change_message: str | list[Any] = "",
+        *,
+        single_object: Literal[False] = ...,
+    ) -> list[LogEntry]: ...
 
 class LogEntry(models.Model):
     action_time: models.DateTimeField
