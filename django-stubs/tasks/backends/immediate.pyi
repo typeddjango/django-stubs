2cb03affcd27ba6bd52307e3b07975bf367ced05
Federico Bond <federicobond@gmail.com>
2025-12-19 14:07:09 -0300

    Add stubs for Django Tasks framework

---
+++ django-stubs/tasks/backends/immediate.pyi
@@ -0,0 +1,13 @@
+from logging import Logger
+from typing import Any
+
+from django.tasks.base import Task, TaskResult
+
+from .base import BaseTaskBackend as BaseTaskBackend
+
+logger: Logger
+
+class ImmediateBackend(BaseTaskBackend):
+    worker_id: str
+    def __init__(self, alias: str, params: dict[str, Any]) -> None: ...
+    def enqueue(self, task: Task, args: list[Any], kwargs: dict[str, Any]) -> TaskResult: ...
