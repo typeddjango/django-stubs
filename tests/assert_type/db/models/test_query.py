0ee9a3887332892ee7b95e5b8b7915f0114540c6
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2025-12-24 20:39:13 +0200

    fix: `prefetch_related_objects` requires `Sequence` not `Iterable`
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- tests/assert_type/db/models/test_query.py
+++ tests/assert_type/db/models/test_query.py
@@ -1,10 +1,7 @@
 from collections.abc import Sequence
 
 from django.db.models import Model
-from django.db.models.query import (
-    aprefetch_related_objects,
-    prefetch_related_objects,
-)  # pyright: ignore[reportUnknownVariableType]
+from django.db.models.query import aprefetch_related_objects, prefetch_related_objects
 
 models_list: list[Model] = []
 prefetch_related_objects(models_list, "pk")
@@ -17,10 +14,10 @@ prefetch_related_objects(models_sequence, "pk")
 
 # failure cases
 models_set: set[Model] = set()
-prefetch_related_objects(models_set, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
+prefetch_related_objects(models_set, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]  # pyrefly: ignore[bad-argument-type]
 
 models_frozenset: frozenset[Model] = frozenset()
-prefetch_related_objects(models_frozenset, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
+prefetch_related_objects(models_frozenset, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]  # pyrefly: ignore[bad-argument-type]
 
 
 async def test_async() -> None:
@@ -29,5 +26,5 @@ async def test_async() -> None:
     await aprefetch_related_objects(models_sequence, "pk")
 
     # failure cases
-    await aprefetch_related_objects(models_set, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
-    await aprefetch_related_objects(models_frozenset, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
+    await aprefetch_related_objects(models_set, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]  # pyrefly: ignore[bad-argument-type]
+    await aprefetch_related_objects(models_frozenset, "pk")  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]  # pyrefly: ignore[bad-argument-type]
