5133316e4a5d5c38a046d65c7c52c625922493fd
Marti Raudsepp <marti@juffo.org>
2026-01-13 19:10:46 +0800

    Revert "Django 6.0: Add stubs for built-in CSP support (#2931)"
    This reverts commit 51beeee9763f2f81dbb48327efb673341feae509.

--- django-stubs/conf/global_settings.pyi
+++ django-stubs/conf/global_settings.pyi
@@ -1,4 +1,4 @@
-from collections.abc import Collection, Mapping, Sequence
+from collections.abc import Sequence
 from re import Pattern
 
 # This is defined here as a do-nothing function because we can't import
@@ -543,9 +543,3 @@ SECURE_REDIRECT_EXEMPT: list[str]
 SECURE_REFERRER_POLICY: str
 SECURE_SSL_HOST: str | None
 SECURE_SSL_REDIRECT: bool
-
-##################
-# CSP MIDDLEWARE #
-##################
-SECURE_CSP: Mapping[str, Collection[str] | str]
-SECURE_CSP_REPORT_ONLY: Mapping[str, Collection[str] | str]
