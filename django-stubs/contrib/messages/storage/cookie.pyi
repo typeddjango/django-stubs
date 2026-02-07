1dd6c03201e825c31e28ed73db92eba910fdf43e
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-07 20:03:03 +0200

    Add missing items across contrib module stubs (#3049)
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- django-stubs/contrib/messages/storage/cookie.pyi
+++ django-stubs/contrib/messages/storage/cookie.pyi
@@ -11,9 +11,11 @@ class MessageEncoder(json.JSONEncoder):
     skipkeys: bool
     sort_keys: bool
     message_key: str
+    def default(self, obj: Any) -> Any: ...
 
 class MessageDecoder(json.JSONDecoder):
     def process_messages(self, obj: Any) -> Any: ...
+    def decode(self, s: str, **kwargs: Any) -> Any: ...  # type: ignore[override]
 
 class MessagePartSerializer:
     def dumps(self, obj: Any) -> Sequence[str]: ...
@@ -29,6 +31,7 @@ class CookieStorage(BaseStorage):
     max_cookie_size: int
     not_finished: str
     not_finished_json: str
+    key_salt: str
 
 def bisect_keep_left(a: list[int], fn: Callable[[list[int]], bool]) -> int: ...
 def bisect_keep_right(a: list[int], fn: Callable[[list[int]], bool]) -> int: ...
