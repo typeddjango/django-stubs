7cf6b2ddad14713c7b708d1b35f1f2e4ad4a1d00
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-13 00:14:22 +0200

    Django 6.0: update SQLite backend stubs
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- django-stubs/db/backends/sqlite3/base.pyi
+++ django-stubs/db/backends/sqlite3/base.pyi
@@ -1,5 +1,7 @@
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
+from datetime import date, datetime
 from sqlite3 import dbapi2 as Database
+from types import ModuleType
 from typing import Any, TypeVar
 
 from django.db.backends.base.base import BaseDatabaseWrapper
@@ -9,10 +11,13 @@ from .creation import DatabaseCreation
 from .features import DatabaseFeatures
 from .introspection import DatabaseIntrospection
 from .operations import DatabaseOperations
+from .schema import DatabaseSchemaEditor
 
 _R = TypeVar("_R")
 
 def decoder(conv_func: Callable[[str], _R]) -> Callable[[bytes], _R]: ...
+def adapt_date(val: date) -> str: ...
+def adapt_datetime(val: datetime) -> str: ...
 
 class DatabaseWrapper(BaseDatabaseWrapper):
     client: DatabaseClient
@@ -21,6 +26,11 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     introspection: DatabaseIntrospection
     ops: DatabaseOperations
 
+    pattern_esc: str
+    pattern_ops: dict[str, str]
+    transaction_modes: frozenset[str]
+    Database: ModuleType
+    SchemaEditorClass: type[DatabaseSchemaEditor]
     client_class: type[DatabaseClient]
     creation_class: type[DatabaseCreation]
     features_class: type[DatabaseFeatures]
@@ -31,4 +41,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
 
 FORMAT_QMARK_REGEX: Any
 
-class SQLiteCursorWrapper(Database.Cursor): ...
+class SQLiteCursorWrapper(Database.Cursor):
+    def execute(self, query: str, params: Iterable[Any] | None = None) -> SQLiteCursorWrapper: ...  # type: ignore[override]
+    def executemany(self, query: str, param_list: Iterable[Iterable[Any]]) -> SQLiteCursorWrapper: ...  # type: ignore[override]
+    def convert_query(self, query: str, *, param_names: Any = None) -> str: ...
--- django-stubs/db/backends/sqlite3/features.pyi
+++ django-stubs/db/backends/sqlite3/features.pyi
@@ -1,12 +1,28 @@
 from django.db.backends.base.features import BaseDatabaseFeatures
 from django.db.backends.sqlite3.base import DatabaseWrapper
+from django.utils.functional import cached_property
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     connection: DatabaseWrapper
+    can_alter_table_drop_column: bool
     supports_aggregate_order_by_clause: bool
     supports_aggregate_distinct_multiple_argument: bool
     supports_any_value: bool
-    @property
-    def can_return_rows_from_update(self) -> bool: ...  # type: ignore[override]
+    @cached_property
+    def django_test_skips(self) -> dict[str, set[str]]: ...  # type: ignore[override]
+    @cached_property
+    def introspected_field_types(self) -> dict[str, str]: ...  # type: ignore[override]
     @property
     def max_query_params(self) -> int: ...  # type: ignore[override]
+    @cached_property
+    def supports_json_field(self) -> bool: ...  # type: ignore[override]
+    @property
+    def can_introspect_json_field(self) -> bool: ...  # type: ignore[override]
+    @property
+    def has_json_object_function(self) -> bool: ...  # type: ignore[override]
+    @cached_property
+    def can_return_columns_from_insert(self) -> bool: ...  # type: ignore[override]
+    @property
+    def can_return_rows_from_bulk_insert(self) -> bool: ...  # type: ignore[override]
+    @property
+    def can_return_rows_from_update(self) -> bool: ...  # type: ignore[override]
