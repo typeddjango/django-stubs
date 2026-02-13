f7b268553e6cb0b294db8a7d0ab6455f6769b2c1
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-13 16:18:13 +0200

    Django 6.0: update MySQL and Oracle backend stubs
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- django-stubs/contrib/gis/db/backends/oracle/introspection.pyi
+++ django-stubs/contrib/gis/db/backends/oracle/introspection.pyi
@@ -1,8 +1,9 @@
+from functools import cached_property
 from typing import Any
 
 from django.db.backends.oracle.introspection import DatabaseIntrospection
 
 class OracleIntrospection(DatabaseIntrospection):
-    @property
-    def data_types_reverse(self) -> Any: ...
+    @cached_property
+    def data_types_reverse(self) -> dict[int, str]: ...  # type: ignore[override]
     def get_geometry_type(self, table_name: Any, description: Any) -> Any: ...
