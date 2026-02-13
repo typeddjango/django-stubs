e36579c291fa10cefe990dfb8046f8029e0c7163
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-13 07:56:01 +0200

    Django 6.0: update SQLite backend stubs (#3072)
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- django-stubs/db/backends/sqlite3/schema.pyi
+++ django-stubs/db/backends/sqlite3/schema.pyi
@@ -1,5 +1,10 @@
 from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 from django.db.backends.sqlite3.base import DatabaseWrapper
+from django.db.models.base import Model
 
 class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     connection: DatabaseWrapper
+    sql_create_fk: None  # type: ignore[assignment]
+    sql_alter_table_comment: None  # type: ignore[assignment]
+    sql_alter_column_comment: None  # type: ignore[assignment]
+    def delete_model(self, model: type[Model], handle_autom2m: bool = True) -> None: ...
