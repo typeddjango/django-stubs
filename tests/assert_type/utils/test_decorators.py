a4385d088d8a49858c02e1d53871742f7c55f88c
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-14 19:06:37 +0200

    Fix conflated decorator and decorator factory return types
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- tests/assert_type/utils/test_decorators.py
+++ tests/assert_type/utils/test_decorators.py
@@ -5,13 +5,13 @@ from django.middleware.cache import CacheMiddleware
 from django.utils.decorators import decorator_from_middleware, decorator_from_middleware_with_args
 from typing_extensions import assert_type
 
-decorator = decorator_from_middleware(CacheMiddleware)
+decorator = decorator_from_middleware(CacheMiddleware)  # pyrefly: ignore[bad-argument-type]
 assert_type(
     decorator,
     Callable[[Callable[..., HttpResponseBase]], Callable[..., HttpResponseBase]],
 )
 
-factory = decorator_from_middleware_with_args(CacheMiddleware)
+factory = decorator_from_middleware_with_args(CacheMiddleware)  # pyrefly: ignore[bad-argument-type]
 assert_type(
     factory,
     Callable[..., Callable[[Callable[..., HttpResponseBase]], Callable[..., HttpResponseBase]]],
