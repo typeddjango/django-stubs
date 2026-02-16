b27cf06d4ab76f184026db1a90dac0d9bd62b242
sobolevn <mail@sobolevn.me>
2026-02-16 12:52:01 +0300

    Add tests for `_PropertyDescriptor` usage in `response.pyi` (#3086)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/assert_type/http/test_response.py
@@ -0,0 +1,31 @@
+from collections.abc import AsyncIterable, AsyncIterator, Iterator
+
+from django.http.response import HttpResponse, StreamingHttpResponse
+from typing_extensions import assert_type
+
+# HttpResponse
+# ============
+
+response = HttpResponse()
+# We can assign any objects, but get bytes back:
+response.content = "abc"
+assert_type(response.content, bytes)
+
+# StreamingHttpResponse
+# =====================
+
+streaming_response = StreamingHttpResponse()
+
+# We can assign any `Iterable`s ...
+streaming_response.streaming_content = [1, 2]
+streaming_response.streaming_content = iter(["a", "b"])
+
+
+def async_iterable() -> AsyncIterable[int]:
+    raise NotImplementedError
+
+
+streaming_response.streaming_content = async_iterable()
+
+# ... but get `Iterator` back:
+assert_type(streaming_response.streaming_content, Iterator[bytes] | AsyncIterator[bytes])
