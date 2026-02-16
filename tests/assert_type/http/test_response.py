12a3e0607160c1710dd81eb4bbeb8e5adbada692
sobolevn <mail@sobolevn.me>
2026-02-16 12:25:43 +0300

    Create test_response.py

---
+++ tests/assert_type/http/test_response.py
@@ -0,0 +1,27 @@
+from collections.abc import Iterable, Iterator, AsyncIterable, AsyncIterator
+
+from django.http.request import HttpResponse, StreamingHttpResponse
+from typing_extensions import assert_type
+
+# HttpResponse
+# ============
+
+response = HttpResponse()
+response.content = 'abc'
+assert_type(response.content, bytes)
+
+# StreamingHttpResponse
+# =====================
+
+streaming_response = StreamingHttpResponse()
+
+# We can assign `Iterator`s ...
+streaming_response.streaming_content = iter([1, 2])
+
+async def async_iterator() -> AsyncIterator[int]:
+    yield 1
+
+streaming_response.streaming_content = async_iterator()
+
+# ... but only get `Iterable` back:
+assert_type(streaming_response.streaming_content, Iterable[object] | AsyncIterable[object])
