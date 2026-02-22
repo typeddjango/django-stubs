6df78bf5039833f831a0028005fa60d979dc5de6
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-02-22 20:27:04 +0100

    Migrate the rest of the file too

--- tests/assert_type/utils/test_encoding.py
+++ tests/assert_type/utils/test_encoding.py
@@ -10,3 +10,33 @@ def test_any_input() -> None:
     assert_type(force_str(x), str)
     assert_type(smart_bytes(x), bytes)
     assert_type(smart_str(x), str)
+
+
+class S(str):
+    pass
+
+
+def test_force_bytes() -> None:
+    assert_type(force_bytes(123), bytes)
+    assert_type(force_bytes(123, strings_only=True), int)
+
+
+def test_force_str() -> None:
+    assert_type(force_str(123), str)
+    assert_type(force_str(123, strings_only=True), int)
+    assert_type(force_str("foo"), str)
+    assert_type(force_str("foo", strings_only=True), str)
+    assert_type(force_str(S("foo"), strings_only=True), S)
+
+
+def test_smart_bytes() -> None:
+    assert_type(smart_bytes(123), bytes)
+    assert_type(smart_bytes(123, strings_only=True), int)
+
+
+def test_smart_str() -> None:
+    assert_type(smart_str(123), str)
+    assert_type(smart_str(123, strings_only=True), int)
+    assert_type(smart_str("foo"), str)
+    assert_type(smart_str("foo", strings_only=True), str)
+    assert_type(smart_str(S("foo"), strings_only=True), S)
