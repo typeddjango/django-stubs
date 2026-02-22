5ea6697061c1fe4b18e9b32819a62c2d093367ec
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-23 00:58:45 +0200

    Add GIS management command stubs (#3107)
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

---
+++ django-stubs/contrib/gis/management/commands/inspectdb.pyi
@@ -0,0 +1,9 @@
+from typing import Any
+
+from django.core.management.commands.inspectdb import Command as InspectDBCommand
+from django.db.backends.base.base import BaseDatabaseWrapper
+
+class Command(InspectDBCommand):
+    def get_field_type(
+        self, connection: BaseDatabaseWrapper, table_name: str, row: Any
+    ) -> tuple[str, dict[str, Any], list[str]]: ...
