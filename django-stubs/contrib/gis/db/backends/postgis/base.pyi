3284868b7a87c2798527d7a6ceaef4f95598cf87
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-02-24 14:24:47 +0530

    added postgis_adapters

--- django-stubs/contrib/gis/db/backends/postgis/base.pyi
+++ django-stubs/contrib/gis/db/backends/postgis/base.pyi
@@ -1,11 +1,10 @@
+from functools import lru_cache
 from typing import Any
 
 from django.db.backends.postgresql.base import DatabaseWrapper as Psycopg2DatabaseWrapper
 from psycopg import BaseConnection
 from psycopg.adapt import Dumper
 from psycopg.pq import Format
-
-class RasterType: ...
 
 class BaseBinaryDumper(Dumper):
     format: Format
@@ -22,3 +21,12 @@ class DatabaseWrapper(Psycopg2DatabaseWrapper):
     def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     def prepare_database(self) -> None: ...
     def register_geometry_adapters(self, pg_connection: BaseConnection[bytes], clear_caches: bool = False) -> None: ...
+
+class RasterType: ...
+
+@lru_cache
+def postgis_adapters(
+    geo_oid: int | None,
+    geog_oid: int | None,
+    raster_oid: int | None,
+) -> tuple[type[Dumper], type[Dumper]]: ...
