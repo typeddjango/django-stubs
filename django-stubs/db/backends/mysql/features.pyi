7c3be008266ba9ec7f233927217533f4d389c3e8
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-27 12:45:22 +0200

    Fix MySQL backend feature stubs
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- django-stubs/db/backends/mysql/features.pyi
+++ django-stubs/db/backends/mysql/features.pyi
@@ -7,43 +7,37 @@ from django.utils.functional import cached_property
 class DatabaseFeatures(BaseDatabaseFeatures):
     connection: DatabaseWrapper
     empty_fetchmany_value: Any
-    allows_group_by_pk: bool
     related_fields_match_type: bool
     allow_sliced_subqueries_with_in: bool
     has_select_for_update: bool
+    has_select_for_update_nowait: bool
+    has_select_for_update_skip_locked: bool
     supports_forward_references: bool
     supports_regex_backreferencing: bool
     supports_date_lookup_using_string: bool
-    can_introspect_autofield: bool
-    can_introspect_binary_field: bool
-    can_introspect_duration_field: bool
-    can_introspect_small_integer_field: bool
-    can_introspect_positive_integer_field: bool
-    introspected_boolean_field_type: str
     supports_timezones: bool
     requires_explicit_null_ordering_when_grouping: bool
-    can_release_savepoints: bool
     atomic_transactions: bool
     can_clone_databases: bool
     supports_temporal_subtraction: bool
-    supports_select_intersection: bool
-    supports_select_difference: bool
     supports_slicing_ordering_in_compound: bool
     supports_index_on_text_field: bool
-    has_case_insensitive_like: bool
+    supports_over_clause: bool
+    supports_frame_range_fixed_distance: bool
     create_test_procedure_without_params_sql: str
     create_test_procedure_with_int_param_sql: str
-    db_functions_convert_bytes_to_str: bool
     supports_partial_indexes: bool
     supports_order_by_nulls_modifier: bool
     order_by_nulls_first: bool
     supports_aggregate_order_by_clause: bool
     supports_json_negative_indexing: bool
 
-    # fake properties (until `property` is generic)
-    supports_frame_range_fixed_distance: bool
-    supports_table_check_constraints: bool
-    can_return_rows_from_bulk_insert: bool
+    @cached_property
+    def minimum_database_version(self) -> tuple[int, ...]: ...  # type: ignore[override]
+    @cached_property
+    def test_collations(self) -> dict[str, str]: ...  # type: ignore[override]
+    @cached_property
+    def django_test_skips(self) -> dict[str, set[str]]: ...  # type: ignore[override]
     @cached_property
     def allows_auto_pk_0(self) -> bool: ...  # type: ignore[override]
     @cached_property
@@ -54,20 +48,18 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     def introspected_field_types(self) -> dict[str, str]: ...  # type: ignore[override]
     @cached_property
     def can_return_columns_from_insert(self) -> bool: ...  # type: ignore[override]
+    @property
+    def can_return_rows_from_bulk_insert(self) -> bool: ...  # type: ignore[override]
     @cached_property
     def has_zoneinfo_database(self) -> bool: ...  # type: ignore[override]
     @cached_property
     def is_sql_auto_is_null_enabled(self) -> bool: ...
     @cached_property
-    def supports_over_clause(self) -> bool: ...  # type: ignore[override]
-    @cached_property
     def supports_column_check_constraints(self) -> bool: ...  # type: ignore[override]
+    @property
+    def supports_table_check_constraints(self) -> bool: ...  # type: ignore[override]
     @cached_property
     def can_introspect_check_constraints(self) -> bool: ...  # type: ignore[override]
-    @cached_property
-    def has_select_for_update_skip_locked(self) -> bool: ...  # type: ignore[override]
-    @cached_property
-    def has_select_for_update_nowait(self) -> bool: ...  # type: ignore[override]
     @cached_property
     def has_select_for_update_of(self) -> bool: ...  # type: ignore[override]
     @cached_property
@@ -88,5 +80,15 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     def supports_index_column_ordering(self) -> bool: ...  # type: ignore[override]
     @cached_property
     def supports_expression_indexes(self) -> bool: ...  # type: ignore[override]
+    @cached_property
+    def supports_select_intersection(self) -> bool: ...  # type: ignore[override]
+    @property
+    def supports_select_difference(self) -> bool: ...  # type: ignore[override]
+    @cached_property
+    def supports_expression_defaults(self) -> bool: ...  # type: ignore[override]
+    @cached_property
+    def has_native_uuid_field(self) -> bool: ...  # type: ignore[override]
+    @cached_property
+    def allows_group_by_selected_pks(self) -> bool: ...  # type: ignore[override]
     @cached_property
     def supports_any_value(self) -> bool: ...  # type: ignore[override]
