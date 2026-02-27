756af7e331c8dc15bf0af9c60423eb6b464d9329
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-27 16:57:18 +0200

    Fix Oracle backend feature stubs (#3136)
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- django-stubs/contrib/gis/db/backends/oracle/features.pyi
+++ django-stubs/contrib/gis/db/backends/oracle/features.pyi
@@ -1,5 +1,6 @@
 from django.contrib.gis.db.backends.base.features import BaseSpatialFeatures
 from django.db.backends.oracle.features import DatabaseFeatures as OracleDatabaseFeatures
+from django.utils.functional import cached_property
 
 class DatabaseFeatures(BaseSpatialFeatures, OracleDatabaseFeatures):
     supports_add_srs_entry: bool
@@ -7,3 +8,7 @@ class DatabaseFeatures(BaseSpatialFeatures, OracleDatabaseFeatures):
     supports_geometry_field_unique_index: bool
     supports_perimeter_geodetic: bool
     supports_dwithin_distance_expr: bool
+    supports_tolerance_parameter: bool
+
+    @cached_property
+    def django_test_skips(self) -> dict[str, set[str]]: ...  # type: ignore[override]
