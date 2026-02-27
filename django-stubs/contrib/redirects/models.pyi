d44173f52ab08a01db7fe813a2f066bd8f8ddcb6
ahmedasar00 <ahmedasartech@outlook.com>
2026-02-27 22:34:14 +0200

    Fix Redirect model stubs: add proper type hints for site, old_path, new_path

--- django-stubs/contrib/redirects/models.pyi
+++ django-stubs/contrib/redirects/models.pyi
@@ -1,6 +1,7 @@
+from django.contrib.sites.models import Site
 from django.db import models
 
 class Redirect(models.Model):
-    site: models.ForeignKey
-    old_path: models.CharField
-    new_path: models.CharField
+    site: models.ForeignKey[Site]
+    old_path: models.CharField[str]
+    new_path: models.CharField[str]
