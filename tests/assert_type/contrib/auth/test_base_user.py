a9f3a8d946ca0b3c31a623fee3b575b12b239b85
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-03-21 15:51:36 +0200

    Add `backend` attribute stub for authenticated users (#3210)
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

---
+++ tests/assert_type/contrib/auth/test_base_user.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from typing_extensions import assert_type
+
+
+def get_backend() -> str:
+    return "django.contrib.auth.backends.ModelBackend"
+
+
+user = User()
+user.backend = get_backend()
+assert_type(user.backend, str)
