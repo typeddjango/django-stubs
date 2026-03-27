6d91189618f9bb7343b40769731c14a11f2cd2bd
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-03-27 23:02:35 +0100

    Move test_utils

--- tests/assert_type/test/test_utils.py
+++ tests/assert_type/test/test_utils.py
@@ -1,6 +1,17 @@
+from collections.abc import Callable
+
+from django.test import override_settings
 from django.test.utils import CaptureQueriesContext
 from typing_extensions import assert_type
 
 
 def test_in_operator(ctx: CaptureQueriesContext, query: dict[str, str]) -> None:
     assert_type(query in ctx, bool)
+
+
+@override_settings(FOO="bar")
+def test() -> None:
+    pass
+
+
+assert_type(test, Callable[[], None])
