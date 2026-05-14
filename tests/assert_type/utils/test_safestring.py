3f6b7511523543502ea93b170ea46559cea28aba
Alexandre Spaeth <Alexerson@users.noreply.github.com>
2026-05-14 16:22:34 -0700

    Fix `mark_safe` to accept `StrOrPromise`
    Closes #3391

--- tests/assert_type/utils/test_safestring.py
+++ tests/assert_type/utils/test_safestring.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from django.utils.safestring import SafeString, mark_safe
+from django.utils.translation import gettext_lazy
 from typing_extensions import assert_type
 
 s = "hello"
@@ -14,6 +15,11 @@ assert_type(s, str)
 ms = mark_safe(s)
 ms += mark_safe(s)
 assert_type(ms, SafeString)
+
+
+# mark_safe accepts lazy strings
+lazy_s = gettext_lazy("hello")
+assert_type(mark_safe(lazy_s), SafeString)
 
 
 # mark_safe as decorator preserves function type
