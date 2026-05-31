30d5d80efecb0a18c8047f7b5106918409f3ef84
sobolevn <mail@sobolevn.me>
2026-05-31 09:29:12 +0300

    Update ty to 0.0.40

--- tests/assert_type/utils/test_encoding.py
+++ tests/assert_type/utils/test_encoding.py
@@ -7,6 +7,9 @@ from django.utils.encoding import force_bytes, force_str, smart_bytes, smart_str
 from typing_extensions import assert_type
 
 
+not_literal_int: int = 123
+
+
 def test_any_input() -> None:
     x: Any = None
     assert_type(force_bytes(x), bytes)
@@ -21,12 +24,12 @@ class S(str):
 
 def test_force_bytes() -> None:
     assert_type(force_bytes(123), bytes)
-    assert_type(force_bytes(123, strings_only=True), int)
+    assert_type(force_bytes(not_literal_int, strings_only=True), int)
 
 
 def test_force_str() -> None:
     assert_type(force_str(123), str)
-    assert_type(force_str(123, strings_only=True), int)
+    assert_type(force_str(not_literal_int, strings_only=True), int)
     assert_type(force_str(dt.time(9, 50), strings_only=True), dt.time)
     assert_type(force_str(dt.date(2026, 5, 31), strings_only=True), dt.date)
     assert_type(force_str(dt.datetime.now(dt.timezone.utc), strings_only=True), dt.datetime)
@@ -37,7 +40,7 @@ def test_force_str() -> None:
 
 def test_smart_bytes() -> None:
     assert_type(smart_bytes(123), bytes)
-    assert_type(smart_bytes(123, strings_only=True), int)
+    assert_type(smart_bytes(not_literal_int, strings_only=True), int)
     assert_type(smart_bytes(dt.time(9, 50), strings_only=True), dt.time)
     assert_type(smart_bytes(dt.date(2026, 5, 31), strings_only=True), dt.date)
     assert_type(smart_bytes(dt.datetime.now(dt.timezone.utc), strings_only=True), dt.datetime)
@@ -45,7 +48,7 @@ def test_smart_bytes() -> None:
 
 def test_smart_str() -> None:
     assert_type(smart_str(123), str)
-    assert_type(smart_str(123, strings_only=True), int)
+    assert_type(smart_str(not_literal_int, strings_only=True), int)
     assert_type(smart_str(dt.time(9, 50), strings_only=True), dt.time)
     assert_type(smart_str(dt.date(2026, 5, 31), strings_only=True), dt.date)
     assert_type(smart_str(dt.datetime.now(dt.timezone.utc), strings_only=True), dt.datetime)
