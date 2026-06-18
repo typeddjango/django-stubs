a84aa0685b87321a0d80e4524a003d9b1ae3a8d4
Brian Helba <brian.helba@kitware.com>
2026-06-17 21:15:36 -0400

    Make `validators` consistent
    * Use `Iterable` for parameters, since the input is never sliced as a sequence
    * Use `list` for attributes, since it's always actually stored as that type

--- django-stubs/db/models/fields/__init__.pyi
+++ django-stubs/db/models/fields/__init__.pyi
@@ -144,7 +144,7 @@ class Field(RegisterLookupMixin, Generic[_ST, _GT]):
     empty_values: Sequence[Any]
     creation_counter: int
     auto_creation_counter: int
-    default_validators: Sequence[validators._ValidatorCallable]
+    default_validators: list[validators._ValidatorCallable]
     default_error_messages: ClassVar[_ErrorMessagesDict]
     hidden: bool
     system_check_removed_details: Any | None
