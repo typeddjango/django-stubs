114ec653c6e3f632cf4c0797b898d71f092e0461
Harshit Pandey <01-Harshit>
2026-06-30 15:56:50 +0530

    Add assert_type test verifying CaptureQueriesContext respects Iterable protocol

--- tests/assert_type/test/test_utils.py
+++ tests/assert_type/test/test_utils.py
@@ -6,6 +6,9 @@ from typing_extensions import assert_type
 def test_in_operator(ctx: CaptureQueriesContext, query: dict[str, str]) -> None:
     assert_type(query in ctx, bool)
 
+def test_iteration(ctx: CaptureQueriesContext) -> None:
+    for query in ctx:
+        assert_type(query, dict[str, str])
 
 @override_settings(FOO="bar")
 def test() -> None:
