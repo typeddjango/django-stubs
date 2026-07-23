53db54ecfbd777811803166a77afdc8fc3769a66
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-07-23 15:39:57 +0200

    Add test case from #1725 converted to assert_type syntax

---
+++ tests/assert_type/apps/test_registry/models.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import Any
+
+from django.apps import apps
+from django.apps.registry import apps as registry_apps
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from typing_extensions import assert_type
+
+
+class First(models.Model):
+    pass
+
+
+class Second(models.Model):
+    pass
+
+
+def get_model_resolves_literal_references() -> None:
+    # Mypy plugin is needed to narrow `type[Any]` to type[MyModel]
+    # TODO: other type checker should also understand that
+    assert_type(apps.get_model("test_registry.First"), type[First])  # pyright: ignore[reportAssertTypeFailure]  # pyrefly: ignore[assert-type]  # ty: ignore[type-assertion-failure]
+    assert_type(apps.get_model("test_registry.first"), type[First])  # pyright: ignore[reportAssertTypeFailure]  # pyrefly: ignore[assert-type]  # ty: ignore[type-assertion-failure]
+    assert_type(apps.get_model("test_registry", "First"), type[First])  # pyright: ignore[reportAssertTypeFailure]  # pyrefly: ignore[assert-type]  # ty: ignore[type-assertion-failure]
+    assert_type(apps.get_model(app_label="test_registry", model_name="first"), type[First])  # pyright: ignore[reportAssertTypeFailure]  # pyrefly: ignore[assert-type]  # ty: ignore[type-assertion-failure]
+    assert_type(apps.get_model(model_name="Second", app_label="test_registry"), type[Second])  # pyright: ignore[reportAssertTypeFailure]  # pyrefly: ignore[assert-type]  # ty: ignore[type-assertion-failure]
+    assert_type(registry_apps.get_model("contenttypes.ContentType"), type[ContentType])  # pyright: ignore[reportAssertTypeFailure]  # pyrefly: ignore[assert-type]  # ty: ignore[type-assertion-failure]
+
+
+def get_model_of_unknown_or_dynamic_reference_is_permissive() -> None:
+    # Unknown models can't be resolved: the mypy plugin reports an error
+    # TODO: other type checker should also raise issues here
+    assert_type(apps.get_model("test_registry.Missing"), type[Any])  # type: ignore[misc]
+    assert_type(apps.get_model("TEST_REGISTRY.First"), type[Any])  # type: ignore[misc]
+    app_label = "test_registry"
+    model = apps.get_model(app_label, "First")
+    assert_type(model, type[Any])
+    model.objects.whatever()
