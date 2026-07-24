0f6e66368f3cea26252959e54107f8b4751f2f74
Thibaut Decombe <68703331+UnknownPlatypus@users.noreply.github.com>
2026-07-24 11:41:50 +0200

    Mark fullnames constants as Final (#3515)

--- mypy_django_plugin/lib/fullnames.py
+++ mypy_django_plugin/lib/fullnames.py
@@ -1,52 +1,54 @@
 from __future__ import annotations
 
-ABSTRACT_BASE_USER_MODEL_FULLNAME = "django.contrib.auth.base_user.AbstractBaseUser"
-ABSTRACT_USER_MODEL_FULLNAME = "django.contrib.auth.models.AbstractUser"
-PERMISSION_MIXIN_CLASS_FULLNAME = "django.contrib.auth.models.PermissionsMixin"
-APPS_FULLNAME = "django.apps.registry.Apps"
-STATE_APPS_FULLNAME = "django.db.migrations.state.StateApps"
-MODEL_METACLASS_FULLNAME = "django.db.models.base.ModelBase"
-MODEL_CLASS_FULLNAME = "django.db.models.base.Model"
-FIELD_FULLNAME = "django.db.models.fields.Field"
-ARRAY_FIELD_FULLNAME = "django.contrib.postgres.fields.array.ArrayField"
-FOREIGN_OBJECT_FULLNAME = "django.db.models.fields.related.ForeignObject"
-FOREIGN_KEY_FULLNAME = "django.db.models.fields.related.ForeignKey"
-ONETOONE_FIELD_FULLNAME = "django.db.models.fields.related.OneToOneField"
-MANYTOMANY_FIELD_FULLNAME = "django.db.models.fields.related.ManyToManyField"
-DUMMY_SETTINGS_BASE_CLASS = "django.conf._DjangoConfLazyObject"
-AUTH_USER_MODEL_FULLNAME = "django.conf.settings.AUTH_USER_MODEL"
+from typing import Final
 
-QUERYSET_CLASS_FULLNAME = "django.db.models.query.QuerySet"
-BASE_MANAGER_CLASS_FULLNAME = "django.db.models.manager.BaseManager"
-MANAGER_CLASS_FULLNAME = "django.db.models.manager.Manager"
-RELATED_MANAGER_CLASS = "django.db.models.fields.related_descriptors.RelatedManager"
-PREFETCH_CLASS_FULLNAME = "django.db.models.query.Prefetch"
-GENERIC_PREFETCH_CLASS_FULLNAME = "django.contrib.contenttypes.prefetch.GenericPrefetch"
+ABSTRACT_BASE_USER_MODEL_FULLNAME: Final = "django.contrib.auth.base_user.AbstractBaseUser"
+ABSTRACT_USER_MODEL_FULLNAME: Final = "django.contrib.auth.models.AbstractUser"
+PERMISSION_MIXIN_CLASS_FULLNAME: Final = "django.contrib.auth.models.PermissionsMixin"
+APPS_FULLNAME: Final = "django.apps.registry.Apps"
+STATE_APPS_FULLNAME: Final = "django.db.migrations.state.StateApps"
+MODEL_METACLASS_FULLNAME: Final = "django.db.models.base.ModelBase"
+MODEL_CLASS_FULLNAME: Final = "django.db.models.base.Model"
+FIELD_FULLNAME: Final = "django.db.models.fields.Field"
+ARRAY_FIELD_FULLNAME: Final = "django.contrib.postgres.fields.array.ArrayField"
+FOREIGN_OBJECT_FULLNAME: Final = "django.db.models.fields.related.ForeignObject"
+FOREIGN_KEY_FULLNAME: Final = "django.db.models.fields.related.ForeignKey"
+ONETOONE_FIELD_FULLNAME: Final = "django.db.models.fields.related.OneToOneField"
+MANYTOMANY_FIELD_FULLNAME: Final = "django.db.models.fields.related.ManyToManyField"
+DUMMY_SETTINGS_BASE_CLASS: Final = "django.conf._DjangoConfLazyObject"
+AUTH_USER_MODEL_FULLNAME: Final = "django.conf.settings.AUTH_USER_MODEL"
 
-CHOICES_CLASS_FULLNAME = "django.db.models.enums.Choices"
-CHOICES_TYPE_METACLASS_FULLNAME = "django.db.models.enums.ChoicesType"
+QUERYSET_CLASS_FULLNAME: Final = "django.db.models.query.QuerySet"
+BASE_MANAGER_CLASS_FULLNAME: Final = "django.db.models.manager.BaseManager"
+MANAGER_CLASS_FULLNAME: Final = "django.db.models.manager.Manager"
+RELATED_MANAGER_CLASS: Final = "django.db.models.fields.related_descriptors.RelatedManager"
+PREFETCH_CLASS_FULLNAME: Final = "django.db.models.query.Prefetch"
+GENERIC_PREFETCH_CLASS_FULLNAME: Final = "django.contrib.contenttypes.prefetch.GenericPrefetch"
 
-WITH_ANNOTATIONS_FULLNAME = "django_stubs_ext.annotations.WithAnnotations"
-ANNOTATIONS_FULLNAME = "django_stubs_ext.annotations.Annotations"
+CHOICES_CLASS_FULLNAME: Final = "django.db.models.enums.Choices"
+CHOICES_TYPE_METACLASS_FULLNAME: Final = "django.db.models.enums.ChoicesType"
 
-ANNOTATED_TYPES_FULLNAMES = {
+WITH_ANNOTATIONS_FULLNAME: Final = "django_stubs_ext.annotations.WithAnnotations"
+ANNOTATIONS_FULLNAME: Final = "django_stubs_ext.annotations.Annotations"
+
+ANNOTATED_TYPES_FULLNAMES: Final = {
     "typing.Annotated",
     "typing_extensions.Annotated",
     WITH_ANNOTATIONS_FULLNAME,
 }
 
 
-BASEFORM_CLASS_FULLNAME = "django.forms.forms.BaseForm"
-FORM_CLASS_FULLNAME = "django.forms.forms.Form"
-MODELFORM_CLASS_FULLNAME = "django.forms.models.ModelForm"
+BASEFORM_CLASS_FULLNAME: Final = "django.forms.forms.BaseForm"
+FORM_CLASS_FULLNAME: Final = "django.forms.forms.Form"
+MODELFORM_CLASS_FULLNAME: Final = "django.forms.models.ModelForm"
 
-FORM_MIXIN_CLASS_FULLNAME = "django.views.generic.edit.FormMixin"
+FORM_MIXIN_CLASS_FULLNAME: Final = "django.views.generic.edit.FormMixin"
 
-REVERSE_ONE_TO_ONE_DESCRIPTOR = "django.db.models.fields.related_descriptors.ReverseOneToOneDescriptor"
-REVERSE_MANY_TO_ONE_DESCRIPTOR = "django.db.models.fields.related_descriptors.ReverseManyToOneDescriptor"
-MANY_TO_MANY_DESCRIPTOR = "django.db.models.fields.related_descriptors.ManyToManyDescriptor"
-MANY_RELATED_MANAGER = "django.db.models.fields.related_descriptors.ManyRelatedManager"
-RELATED_FIELDS_CLASSES = frozenset(
+REVERSE_ONE_TO_ONE_DESCRIPTOR: Final = "django.db.models.fields.related_descriptors.ReverseOneToOneDescriptor"
+REVERSE_MANY_TO_ONE_DESCRIPTOR: Final = "django.db.models.fields.related_descriptors.ReverseManyToOneDescriptor"
+MANY_TO_MANY_DESCRIPTOR: Final = "django.db.models.fields.related_descriptors.ManyToManyDescriptor"
+MANY_RELATED_MANAGER: Final = "django.db.models.fields.related_descriptors.ManyRelatedManager"
+RELATED_FIELDS_CLASSES: Final = frozenset(
     (
         FOREIGN_OBJECT_FULLNAME,
         FOREIGN_KEY_FULLNAME,
@@ -54,23 +56,23 @@ RELATED_FIELDS_CLASSES = frozenset(
     )
 )
 
-OPTIONS_CLASS_FULLNAME = "django.db.models.options.Options"
-QUERYDICT_CLASS_FULLNAME = "django.http.request.QueryDict"
+OPTIONS_CLASS_FULLNAME: Final = "django.db.models.options.Options"
+QUERYDICT_CLASS_FULLNAME: Final = "django.http.request.QueryDict"
 
-COMBINABLE_EXPRESSION_FULLNAME = "django.db.models.expressions.Combinable"
-F_EXPRESSION_FULLNAME = "django.db.models.expressions.F"
-FUNC_EXPRESSION_FULLNAME = "django.db.models.expressions.Func"
-AGGREGATE_CLASS_FULLNAME = "django.db.models.aggregates.Aggregate"
+COMBINABLE_EXPRESSION_FULLNAME: Final = "django.db.models.expressions.Combinable"
+F_EXPRESSION_FULLNAME: Final = "django.db.models.expressions.F"
+FUNC_EXPRESSION_FULLNAME: Final = "django.db.models.expressions.Func"
+AGGREGATE_CLASS_FULLNAME: Final = "django.db.models.aggregates.Aggregate"
 
-ANY_ATTR_ALLOWED_CLASS_FULLNAME = "django_stubs_ext.AnyAttrAllowed"
-TYPED_MODEL_META_FULLNAME = "django_stubs_ext.db.models.TypedModelMeta"
-STR_PROMISE_FULLNAME = "django.utils.functional._StrPromise"
+ANY_ATTR_ALLOWED_CLASS_FULLNAME: Final = "django_stubs_ext.AnyAttrAllowed"
+TYPED_MODEL_META_FULLNAME: Final = "django_stubs_ext.db.models.TypedModelMeta"
+STR_PROMISE_FULLNAME: Final = "django.utils.functional._StrPromise"
 
-OBJECT_DOES_NOT_EXIST = "django.core.exceptions.ObjectDoesNotExist"
-OBJECT_NOT_UPDATED = "django.core.exceptions.ObjectNotUpdated"
-MULTIPLE_OBJECTS_RETURNED = "django.core.exceptions.MultipleObjectsReturned"
+OBJECT_DOES_NOT_EXIST: Final = "django.core.exceptions.ObjectDoesNotExist"
+OBJECT_NOT_UPDATED: Final = "django.core.exceptions.ObjectNotUpdated"
+MULTIPLE_OBJECTS_RETURNED: Final = "django.core.exceptions.MultipleObjectsReturned"
 
-DJANGO_ABSTRACT_MODELS = frozenset(
+DJANGO_ABSTRACT_MODELS: Final = frozenset(
     (
         ABSTRACT_BASE_USER_MODEL_FULLNAME,
         ABSTRACT_USER_MODEL_FULLNAME,
