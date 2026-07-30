e380fdfc0eb52fd0bbb53324f7f2bd6a3da72b5f
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-07-30 12:02:53 +0200

    Address review: use bare float and drop redundant int test cases

--- tests/assert_type/utils/test_translation.py
+++ tests/assert_type/utils/test_translation.py
@@ -4,10 +4,8 @@ from django.utils.functional import _StrPromise
 from django.utils.translation import ngettext, ngettext_lazy, npgettext, npgettext_lazy
 from typing_extensions import assert_type
 
-# ngettext / npgettext accept int and float
-assert_type(ngettext("apple", "apples", 1), str)
+# ngettext / npgettext accept float
 assert_type(ngettext("apple", "apples", 1.0), str)
-assert_type(npgettext("fruit", "apple", "apples", 1), str)
 assert_type(npgettext("fruit", "apple", "apples", 1.0), str)
 
 # lazy variants also accept str and None (deferred number resolution)
