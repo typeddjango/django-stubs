f4801cb5176367efa738614efea7b0a4867bb1ca
Brian Helba <brian.helba@kitware.com>
2026-08-04 22:42:02 -0400

    Add overloads for `assertTemplateUsed` and `assertTemplateNotUsed`
    The return type of these can actually be
    determined more precisely, given the argument types.
    
    Follows https://github.com/pytest-dev/pytest-django/pull/12781

--- tests/assert_type/test/test_testcase.py
+++ tests/assert_type/test/test_testcase.py
@@ -4,7 +4,7 @@ from typing import assert_type
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.test.client import Client
-from django.test.testcases import TestCase
+from django.test.testcases import TestCase, _AssertTemplateNotUsedContext, _AssertTemplateUsedContext
 
 
 class ExampleTestCase(TestCase):
@@ -14,3 +14,19 @@ class ExampleTestCase(TestCase):
         assert_type(resp.status_code, int)
         resp.json()
         assert_type(resp.wsgi_request, WSGIRequest)
+
+    def test_assert_template_used(self) -> None:
+        response = self.client.get("/url")
+        assert_type(self.assertTemplateUsed(response, "template.html"), None)
+        with self.assertTemplateUsed("template.html") as ctx:
+            assert_type(ctx, _AssertTemplateUsedContext)
+        with self.assertTemplateUsed(template_name="template.html") as ctx:
+            assert_type(ctx, _AssertTemplateUsedContext)
+
+    def test_assert_template_not_used(self) -> None:
+        response = self.client.get("/url")
+        assert_type(self.assertTemplateNotUsed(response, "template.html"), None)
+        with self.assertTemplateNotUsed("template.html") as ctx:
+            assert_type(ctx, _AssertTemplateNotUsedContext)
+        with self.assertTemplateNotUsed(template_name="template.html") as ctx:
+            assert_type(ctx, _AssertTemplateNotUsedContext)
