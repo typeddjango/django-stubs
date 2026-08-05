4408b2ea82248870a931e9538e21b99e25eb91ce
Nick Pope <nick@nickpope.me.uk>
2026-08-06 00:18:13 +0100

    Attempt to stop complaints for tests against Django 5.2

--- tests/assert_type/contrib/test_contenttypes/models.py
+++ tests/assert_type/contrib/test_contenttypes/models.py
@@ -1,20 +1,26 @@
 from __future__ import annotations
 
+import sys
 from typing import Any
 
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericForeignKeyDescriptor
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from typing_extensions import assert_type
 
+# `GenericForeignKeyDescriptor` is only available in Django 6.1+ but we test against Django 5.2.x
+# on Python < 3.12, so only run these tests for newer Python versions to avoid lots of issues with
+# different type checkers complaining when attempting to fall back to `GenericForeignKey`
+# when `ImportError` is raised.
+if sys.version_info >= (3, 12):
+    from django.contrib.contenttypes.fields import GenericForeignKeyDescriptor
 
-class TaggedItem(models.Model):
-    tag = models.SlugField()  # pyright: ignore[reportUnknownVariableType]
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # pyright: ignore[reportUnknownVariableType]
-    object_id = models.PositiveBigIntegerField()  # pyright: ignore[reportUnknownVariableType]
-    content_object = GenericForeignKey("content_type", "object_id")
+    class TaggedItem(models.Model):
+        tag = models.SlugField()  # pyright: ignore[reportUnknownVariableType]
+        content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # pyright: ignore[reportUnknownVariableType]
+        object_id = models.PositiveBigIntegerField()  # pyright: ignore[reportUnknownVariableType]
+        content_object = GenericForeignKey("content_type", "object_id")
 
-
-def test_generic_foreign_key_descriptor() -> None:
-    assert_type(TaggedItem().content_object, Any | None)  # pyrefly: ignore[assert-type]
-    assert_type(TaggedItem.content_object, GenericForeignKeyDescriptor)  # pyrefly: ignore[assert-type]
+    def test_generic_foreign_key_descriptor() -> None:
+        assert_type(TaggedItem().content_object, Any | None)  # pyrefly: ignore[assert-type]
+        assert_type(TaggedItem.content_object, GenericForeignKeyDescriptor)  # pyrefly: ignore[assert-type]
