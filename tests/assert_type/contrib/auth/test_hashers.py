3e96d641602bbd2c78c7155de9185ed1ed0cd307
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2026-09-03 16:52:41 +0200

    Type setter of hashers.check_password better (#3621)

--- tests/assert_type/contrib/auth/test_hashers.py
+++ tests/assert_type/contrib/auth/test_hashers.py
@@ -8,9 +8,13 @@ from django.contrib.auth.hashers import check_password
 def setter(password: str | None) -> None: ...
 
 
+def perfect_setter(password: str) -> None: ...
+
+
 def bad_setter(password: int) -> None: ...
 
 
 assert_type(check_password("password", "encoded"), bool)
 assert_type(check_password("password", "encoded", setter), bool)
+assert_type(check_password("password", "encoded", perfect_setter), bool)
 check_password("password", "encoded", bad_setter)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]  # pyrefly: ignore[bad-argument-type]  # ty: ignore[invalid-argument-type]
