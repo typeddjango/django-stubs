da8322951fdc7ef301b9be30ac868e7ddf691c13
Jonas Dittrich <kakadus2303@gmail.com>
2026-09-03 20:08:42 +0200

    revert test

--- tests/assert_type/contrib/auth/test_models.py
+++ tests/assert_type/contrib/auth/test_models.py
@@ -3,28 +3,14 @@ from __future__ import annotations
 from typing import assert_type
 
 from django.contrib.auth.models import AbstractBaseUser, UserManager
-from typing_extensions import override
 
 
 class MyUser(AbstractBaseUser):
-    @override
-    def natural_key(self) -> tuple[str, str]:
-        return (self.get_username(), self.password)
+    pass
 
 
 class Manager(UserManager[MyUser]):
     pass
 
 
-class MyOtherUser(AbstractBaseUser):
-    @override
-    def natural_key(self) -> tuple[()]:
-        return ()
-
-
-class NonUniqueManager(UserManager[MyOtherUser]):
-    pass
-
-
 assert_type(Manager().create_user("username"), MyUser)
-assert_type(NonUniqueManager().create_user("username"), MyOtherUser)
