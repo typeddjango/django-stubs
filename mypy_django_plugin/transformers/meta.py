03476a95cf0e82649c806a0e3d25eebc3b44b729
Federico Bond <federicobond@gmail.com>
2026-09-07 06:44:25 +1000

    Defer `_meta.get_field()` when the model is defined later in the module (#3489)
    Fixes #2833

--- mypy_django_plugin/transformers/meta.py
+++ mypy_django_plugin/transformers/meta.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from django.core.exceptions import FieldDoesNotExist
+from mypy.nodes import Var
 from mypy.types import AnyType, Instance, TypeOfAny, get_proper_type
 from mypy.types import Type as MypyType
 
@@ -27,6 +28,15 @@ def return_proper_field_type_from_get_field(ctx: MethodContext, django_context: 
     field_type = get_field_type_from_model_type_info(model_type.type, field_name)
     if field_type is not None:
         return field_type
+
+    # Fields of a model defined further down the module aren't inferred yet; defer to a later pass.
+    field_sym = model_type.type.get(field_name)
+    if field_sym is not None and isinstance(field_sym.node, Var) and not field_sym.node.is_ready:
+        typechecker_api = helpers.get_typechecker_api(ctx)
+        # Deferral re-checks the enclosing top-level function, so without one there is nothing to defer.
+        if typechecker_api.scope.top_level_function() is not None:
+            typechecker_api.handle_cannot_determine_type(field_name, ctx.context)
+            return ctx.default_return_type
 
     if (django_model := DjangoModel.from_model_type(model_type, django_context)) is None:
         return ctx.default_return_type
